ffb57813e80dfed67ee197f49d49fbeeedd6b5aa
madanus <madanus@collab.net>
2008-02-28 08:15:40 +0000

    Removed dependencies on the td2jira Config.java Removed the remaining unused td2jira classes and files
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk@48 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- src/plugins/HPQC/com/collabnet/ccf/pi/qc/api/ConnectionFactory.java
+++ src/plugins/HPQC/com/collabnet/ccf/pi/qc/api/ConnectionFactory.java
@@ -2,13 +2,11 @@ package com.collabnet.ccf.pi.qc.api;
 
 import com.collabnet.ccf.pi.qc.api.dcom.Connection;
 
-import td2jira.Config;
-
 public class ConnectionFactory {
 	private static IConnection instance ;
 	public static IConnection getInstance(String server, String domain, String project, String user, String pass) {
 		if( instance == null || !instance.isLoggedIn() ) {
-			String clz = Config.TD_IMPLEMENTATION;
+			String clz = "com.collabnet.ccf.pi.qc.api.dcom.Connection";
 			if( clz == null || clz.trim().length() == 0 ) clz = Connection.class.getCanonicalName();
 			try {
 				Class[] types = new Class[]{String.class,String.class,String.class,String.class,String.class};
