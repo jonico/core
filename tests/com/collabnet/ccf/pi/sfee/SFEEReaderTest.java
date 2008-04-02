3a6e7693159116edc81f3e3ec66e5d4457965bba
mseethar <mseethar@collab.net>
2008-04-02 13:50:42 +0000

    JUnit test case for SFEEReader
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk@146 6771e1e2-0f46-0410-b5d7-d846ff89f743

---
+++ tests/com/collabnet/ccf/pi/sfee/SFEEReaderTest.java
@@ -0,0 +1,49 @@
+package com.collabnet.ccf.pi.sfee;
+
+import junit.framework.TestCase;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import com.collabnet.ccf.pi.sfee.SFEEToGenericArtifactConverter;
+
+public class SFEEReaderTest extends TestCase {
+	SFEEReader sfeeReader = null;
+	String username="mseethar";
+    String password="password";
+    String serverUrl="http://cu074.cubit.maa.collab.net:8080";
+    String keepAlive = "true";
+    SFEEToGenericArtifactConverter artifactConverter = new SFEEToGenericArtifactConverter();
+	public void setUp() throws Exception {
+		super.setUp();
+		sfeeReader = new SFEEReader();
+		sfeeReader.setUsername(username);
+		sfeeReader.setPassword(password);
+		sfeeReader.setServerUrl(serverUrl);
+		sfeeReader.setKeepAlive(keepAlive);
+		sfeeReader.setArtifactConverter(artifactConverter);
+		sfeeReader.validate(new ArrayList());
+	}
+
+	public void testReadTrackerItems(){
+		String projectTracker = "tracker1001";
+		//"Mon Nov 05 00:00:00 GMT+05:30 2007"
+		Date lastModifiedDate = new Date(2007-1900,10,05,0,0,0);
+		String lastArtifactId = "artf1001";
+		int lastArtifactVersion = -1;
+		boolean firstTimeImport = false;
+		try {
+			sfeeReader.connect();
+			sfeeReader.readTrackerItems(projectTracker, lastModifiedDate, lastArtifactId, lastArtifactVersion, firstTimeImport, null);
+		} catch (IOException e) {
+			fail(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
+	public void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
