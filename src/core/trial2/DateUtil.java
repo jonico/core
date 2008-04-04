d39c0d6b2c9112cb1170a4fbdbdb4b2327b8f98a
venugopala <venugopala@collab.net>
2008-04-04 05:50:59 +0000

    trial2.DateUtil:
    Added methods to help QC timeFormats.
    
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk@159 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- src/core/trial2/DateUtil.java
+++ src/core/trial2/DateUtil.java
@@ -9,7 +9,26 @@ import com.collabnet.ccf.core.ga.GenericArtifactHelper;
 
 public class DateUtil {
 	private static final DateFormat dateFormat = GenericArtifactHelper.df;
+	private static final SimpleDateFormat qcDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	
+	//Conversion of String into Date for QC
+	public static Date parseQCDate(String dateString){
+		return parse(dateString, qcDateFormat);
+	}
+	//Conversion of Date into String for QC
+	public static String formatQCDate(Date date){
+		return format(date, qcDateFormat);
+	}
+
 	public static Date parse(String dateString){
+		return parse(dateString, dateFormat);
+	}
+	
+	public static String format(Date date){
+		return format(date, dateFormat);
+	}
+	
+	public static Date parse(String dateString, DateFormat dateFormat){
 		try {
 			return dateFormat.parse(dateString);
 		} catch (ParseException e) {
@@ -18,7 +37,7 @@ public class DateUtil {
 		return null;
 	}
 	
-	public static String format(Date date){
+	public static String format(Date date, DateFormat dateFormat){
 		return dateFormat.format(date);
 	}
 }
