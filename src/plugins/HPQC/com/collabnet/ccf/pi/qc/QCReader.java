2467ba718f5f4cea6a33d59a9e9fb3fb60ed60a0
venugopala <venugopala@collab.net>
2008-04-04 05:13:16 +0000

    com.collabnet.ccf.pi.qc.QCReader:
    - Changed the model from time-based selection to Transaction Id based selection
    
    - Updation of the last transaction in the Repository Mapping table.
    
    
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk@150 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- src/plugins/HPQC/com/collabnet/ccf/pi/qc/QCReader.java
+++ src/plugins/HPQC/com/collabnet/ccf/pi/qc/QCReader.java
@@ -1,7 +1,12 @@
 package com.collabnet.ccf.pi.qc;
 
-import java.io.IOException;
 import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -47,6 +52,7 @@ public class QCReader extends QCConnectHelper implements
 		if(document!=null) log.info(document.getText());
 		try {
 			connect();
+			qcc.connectProjectEx(getDomain(), getProjectName(), getUserName(), getPassword());
 		} catch (Exception e) {
 			// TODO Declare exception so that it can be processed by OA exception handler
 			log.error("Could not log into QC", e);
@@ -54,9 +60,9 @@ public class QCReader extends QCConnectHelper implements
 		}
 
 		// Fix these time operations
-		String fromTime = getFromTime(document);
-		String toTime = getToTime(document);
-		
+		String fromTimestamp = getFromTime(document);
+		String fromTime = convertIntoString(fromTimestamp);
+		String transactionId = getTransactionId(document);
 		String sourceArtifactId = getSourceArtifactId(document); 
 		String sourceRepositoryId = getSourceRepositoryId(document);
 		String sourceRepositoryKind = getSourceRepositoryKind(document);
@@ -69,8 +75,7 @@ public class QCReader extends QCConnectHelper implements
 		String targetSystemKind = getTargetSystemKind(document);
 		
 		log.error(fromTime);
-		log.error(toTime);
-		Object[] result=readModifiedDefects(fromTime, toTime, sourceArtifactId, sourceRepositoryId, sourceRepositoryKind, sourceSystemId, sourceSystemKind, targetRepositoryId, targetRepositoryKind, targetSystemId, targetSystemKind);
+		Object[] result=readModifiedDefects(transactionId, sourceArtifactId, sourceRepositoryId, sourceRepositoryKind, sourceSystemId, sourceSystemKind, targetRepositoryId, targetRepositoryKind, targetSystemId, targetSystemKind);
 		disconnect();
 		return result;
 	}
@@ -90,7 +95,9 @@ public class QCReader extends QCConnectHelper implements
 		isDry=false;
 	}
 	
-	public Object[] readModifiedDefects(String fromTime, String toTime, String sourceArtifactId, String sourceRepositoryId, String sourceRepositoryKind, String sourceSystemId, String sourceSystemKind, String targetRepositoryId, String targetRepositoryKind, String targetSystemId, String targetSystemKind) {
+	
+	
+	public Object[] readModifiedDefects(String transactionId, String sourceArtifactId, String sourceRepositoryId, String sourceRepositoryKind, String sourceSystemId, String sourceSystemKind, String targetRepositoryId, String targetRepositoryKind, String targetSystemId, String targetSystemKind) {
 		// TODO Use the information of the firstTimeImport flag
 		
 		//Object[] retObj = new Object[100];
@@ -99,8 +106,8 @@ public class QCReader extends QCConnectHelper implements
 		List<GenericArtifact> defectRows;
 		
 		try {
-			log.error("The fromTime coming from HQSL DB is:" + fromTime + " and the toTime is" +toTime);
-			defectRows = defectHandler.getChangedDefects(this.getQcc(), fromTime, toTime, sourceArtifactId, sourceRepositoryId, sourceRepositoryKind, sourceSystemId, sourceSystemKind, targetRepositoryId, targetRepositoryKind, targetSystemId, targetSystemKind);
+			log.error("The transactionId coming from HQSL DB is:" + transactionId);
+			defectRows = defectHandler.getChangedDefects(this.getQcc(), transactionId, sourceArtifactId, sourceRepositoryId, sourceRepositoryKind, sourceSystemId, sourceSystemKind, targetRepositoryId, targetRepositoryKind, targetSystemId, targetSystemKind);
 		} catch (Exception e) {
 			// TODO Throw an exception?
 			log.error("During the artifact retrieval process from QC, an error occured",e);
@@ -111,8 +118,10 @@ public class QCReader extends QCConnectHelper implements
 
 			// Return an empty generic artifact
 			// TODO: Should we fill in the latest from and to time?
-			GenericArtifact emptyGenericArtifact = new GenericArtifact();
 			Document document = null;
+			GenericArtifact emptyGenericArtifact = new GenericArtifact();
+			emptyGenericArtifact = populateRequiredFields(emptyGenericArtifact);
+			
 			try {
 				document = GenericArtifactHelper.createGenericArtifactXMLDocument(emptyGenericArtifact);
 			}
@@ -120,18 +129,18 @@ public class QCReader extends QCConnectHelper implements
 				// TODO: Handle this appropriately
 				log.error("GenericArtifactParsingException while creating a GenericArtifact XML from an empty GenericArtifact object");
 			}
+			
 			return new Object[]{document};
 		}
 		 
-		/*try {
-		FileOutputStream f = new FileOutputStream("resultantGenericArtifact.xml"); 
-		ObjectOutputStream s = new ObjectOutputStream(f);
-		*/
+		//String newFromTime = getLastModifiedDateFromLastArtifact(defectRows);
+		//Boolean updateStatus = updateFromTimeInHsqlTable(newFromTime, sourceRepositoryId, sourceRepositoryKind, sourceSystemId, sourceSystemKind, targetRepositoryId, targetRepositoryKind, targetSystemId, targetSystemKind);		
+		
+		String newTransactionId = getLastTransactionIdFromLastArtifact(defectRows);
+		Boolean updateStatus = updateTransactionIdInHsqlTable(newTransactionId, sourceRepositoryId, sourceRepositoryKind, sourceSystemId, sourceSystemKind, targetRepositoryId, targetRepositoryKind, targetSystemId, targetSystemKind);		
 		
 		for (GenericArtifact defectRow: defectRows) {
 			Document document = null;
-			
-
 			try {
 				document = GenericArtifactHelper.createGenericArtifactXMLDocument(defectRow);
 			}
@@ -168,15 +177,114 @@ public class QCReader extends QCConnectHelper implements
 		
 		defectHandler = new QCDefectHandler();
 	}
-
-	private String getToTime(Document document) {
+	public GenericArtifact populateRequiredFields(GenericArtifact emptyGenericArtifact) {
+		
+		
+		emptyGenericArtifact = QCConfigHelper.getSchemaFields(qcc);
+		emptyGenericArtifact.setArtifactMode(GenericArtifact.ArtifactModeValue.UNKNOWN);
+		emptyGenericArtifact.setArtifactType(GenericArtifact.ArtifactTypeValue.UNKNOWN);
+		emptyGenericArtifact.setArtifactAction(GenericArtifact.ArtifactActionValue.UNKNOWN);
+		
+		/*List<GenericArtifactField> allFields = emptyGenericArtifact.getAllGenericArtifactFields();
+		for (GenericArtifactField thisField : allFields) {
+			thisField.setFieldAction(GenericArtifactField.FieldActionValue.UNKNOWN);
+		}
+		*/
+		return emptyGenericArtifact;
+	}
+	
+	public String getLastTransactionIdFromLastArtifact(List<GenericArtifact> defectRows) {
+		
+		String newTransactionId = new String();
+		int size = defectRows.size();
+		GenericArtifact thisArtifact = defectRows.get(size-1);
+		newTransactionId = thisArtifact.getLastReadTransactionId();
+		return newTransactionId;
+	}
+	
+	public Timestamp convertIntoTimestamp(String newFromTime) {
+		Timestamp fromDateInTimestamp=null;
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			Date fromDate = sdf.parse(newFromTime); //like 2008-01-11 22:09:54
+			fromDateInTimestamp = new Timestamp(fromDate.getTime());
+			log.info("After parsing, the timestamp is:"+fromDateInTimestamp);
+			}
+		catch(Exception e) {
+			log.error("Exception while parsing the string into Date"+e);
+		}
+		return fromDateInTimestamp;
+	}
+	
+	public String convertIntoString(String fromTimeStamp) {
+		String finalString=null;
+		try {
+		SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+		Date fromDate = sdf.parse(fromTimeStamp);
+		finalString=ft.format(fromDate);
+		System.out.println(finalString);
+		}
+		catch(Exception e) {
+			log.error("Exception while parsing the string into Date"+e);
+		}
+		return finalString;		
+	}
+	
+	public boolean updateTransactionIdInHsqlTable(String newTransactionId, String sourceRepositoryId, String sourceRepositoryKind, String sourceSystemId, String sourceSystemKind, String targetRepositoryId, String targetRepositoryKind, String targetSystemId, String targetSystemKind) {
+		
+		Boolean status=true;
+		//Timestamp fromDateInTimestamp=convertIntoTimestamp(newFromTime);
+		
+		String sql = "UPDATE REPOSITORY_MAPPING SET TRANSACTION_ID="+newTransactionId; 
+			sql+= " WHERE ";
+			
+			sql+= "REPOSITORY_MAPPING.SOURCE_REPOSITORY_INFO_ID ="; 
+			sql+= "(SELECT ID FROM REPOSITORY_INFO WHERE ";
+			sql+= "REPOSITORY_INFO.REPOSITORY_ID='"+sourceRepositoryId+"' AND ";
+			sql+= "REPOSITORY_INFO.REPOSITORY_KIND='"+sourceRepositoryKind+"' AND ";			
+			sql+= "REPOSITORY_INFO.SYSTEM_INFO_ID= ";
+			sql+= "(SELECT ID FROM SYSTEM_INFO WHERE ";
+			sql+= "SYSTEM_INFO.SYSTEM_ID='"+sourceSystemId+"' AND ";
+			sql+= "SYSTEM_INFO.SYSTEM_KIND='"+sourceSystemKind+"' )) AND ";
+			
+			sql+= "REPOSITORY_MAPPING.TARGET_REPOSITORY_INFO_ID = "; 
+			sql+= "(SELECT ID FROM REPOSITORY_INFO WHERE ";
+			sql+= "REPOSITORY_INFO.REPOSITORY_ID='"+targetRepositoryId+"' AND ";
+			sql+= "REPOSITORY_INFO.REPOSITORY_KIND='"+targetRepositoryKind+"' AND ";
+			sql+= "REPOSITORY_INFO.SYSTEM_INFO_ID= ";
+			sql+= "(SELECT ID FROM SYSTEM_INFO WHERE ";
+			sql+= "SYSTEM_INFO.SYSTEM_ID='"+targetSystemId+"' AND ";
+			sql+= "SYSTEM_INFO.SYSTEM_KIND='"+targetSystemKind+"' ))";
+		log.info(sql);	
+		ResultSet rs = null;
+		try {
+			rs = executeSql(sql);
+		}
+		catch(Exception e) {
+			log.error("Exception while executing the UPDATE Query in QCReader:"+e);
+		}
+		if(rs==null) return false;	
+		return status;
+	}
+	
+	
+	
+	
+	private String getVersion(Document document) {
 		// TODO Let the user specify this value?
-		Node node= document.selectSingleNode("//TO_TIME");
+		Node node= document.selectSingleNode("//VERSION");
 		if (node==null)
 			return null;
 		return node.getText();
 	}
-
+	private String getTransactionId(Document document) {
+		// TODO Let the user specify this value?
+		Node node= document.selectSingleNode("//TRANSACTION_ID");
+		if (node==null)
+			return null;
+		return node.getText();
+	}	
 	private String getFromTime(Document document) {
 		// TODO Let the user specify this value?
 		Node node= document.selectSingleNode("//FROM_TIME");
@@ -251,7 +359,16 @@ public class QCReader extends QCConnectHelper implements
 			return null;
 		return node.getText();
 	}
-	
+	public static ResultSet executeSql(String sql) throws ClassNotFoundException, SQLException {
+	  	Connection conn;                                                
+        Class.forName("org.hsqldb.jdbcDriver");
+        conn = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/xdb", "sa", "");
+        Statement st = null;
+        ResultSet rs = null;
+        st = conn.createStatement();         // statement objects can be reused with
+        rs = st.executeQuery(sql);    // run the query
+        return rs;
+  }
 	
 	
 	public void reset(Object context) {
