6624afebadac5a4ccc65c160671fafea758dbb6e
mseethar <mseethar@collab.net>
2008-04-11 12:49:20 +0000

    Interface change to detect the field types in SFEE.
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk@197 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- src/plugins/SFEE/com/collabnet/ccf/pi/sfee/IArtifactToGAConverter.java
+++ src/plugins/SFEE/com/collabnet/ccf/pi/sfee/IArtifactToGAConverter.java
@@ -1,6 +1,7 @@
 package com.collabnet.ccf.pi.sfee;
 
 import com.collabnet.ccf.core.ga.GenericArtifact;
+import com.vasoftware.sf.soap44.webservices.sfmain.TrackerFieldSoapDO;
 public interface IArtifactToGAConverter {
-	public GenericArtifact convert(Object dataObject);
+	public GenericArtifact convert(Object dataObject, TrackerFieldSoapDO[] trackerFields);
 }
