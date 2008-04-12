391b0dc2df0973104fdee41cd8a772358d374c75
venugopala <venugopala@collab.net>
2008-04-12 06:44:11 +0000

    Updating the Transaction ID in HSQL Repository Mapping Table
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk@228 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- src/core/trial2/MappingDBUpdater.java
+++ src/core/trial2/MappingDBUpdater.java
@@ -27,7 +27,17 @@ public class MappingDBUpdater implements IDataProcessor{
 					+" REPOSITORY_MAPPING.ID=ARTIFACT_MAPPING.MAPPING_ID AND"
 					+" ARTIFACT_MAPPING.SOURCE_ARTIFACT_ID=? AND"
 					+" ARTIFACT_MAPPING.TARGET_ARTIFACT_ID=?)";
-			
+	
+	String qcSql = "UPDATE REPOSITORY_MAPPING SET TRANSACTION_ID=? WHERE ID=(SELECT REPOSITORY_MAPPING.ID" 
+		+" FROM REPOSITORY_MAPPING, REPOSITORY_INFO A, REPOSITORY_INFO B, ARTIFACT_MAPPING WHERE"
+		+" A.ID=REPOSITORY_MAPPING.SOURCE_REPOSITORY_INFO_ID AND"
+		+" B.ID=REPOSITORY_MAPPING.TARGET_REPOSITORY_INFO_ID AND"
+		+" A.REPOSITORY_ID=? AND"
+		+" B.REPOSITORY_ID=? AND"
+		+" REPOSITORY_MAPPING.ID=ARTIFACT_MAPPING.MAPPING_ID AND"
+		+" ARTIFACT_MAPPING.SOURCE_ARTIFACT_ID=? AND"
+		+" ARTIFACT_MAPPING.TARGET_ARTIFACT_ID=?)";
+	
 	public Object[] process(Object data) {
 		// I will expect a Generic Artifact object
 		if(data instanceof Document){
@@ -38,7 +48,14 @@ public class MappingDBUpdater implements IDataProcessor{
 			} catch (GenericArtifactParsingException e1) {
 				throw new RuntimeException(e1);
 			}
+			
+			if(ga.getArtifactAction().equals(GenericArtifact.ArtifactActionValue.UNKNOWN)) {
+				Object [] result = {data};
+				return result;
+			}
+			
 			String lastModifiedDateString = ga.getArtifactLastModifiedDate();
+			String lastReadTransactionId = ga.getLastReadTransactionId();
 			String sourceRepositoryID = ga.getSourceRepositoryId();
 			String targetRepositoryID = ga.getTargetRepositoryId();
 			String sourceArtifactID = ga.getSourceArtifactId();
@@ -67,6 +84,43 @@ public class MappingDBUpdater implements IDataProcessor{
 				pstmt.setString(4, sourceArtifactID);
 				pstmt.setString(5, targetArtifactID);
 				int numRecordsAffected = pstmt.executeUpdate();
+				if(numRecordsAffected != 1){
+					dbConnection.rollback();
+					throw new RuntimeException("How come I am updating two repository mapping....?");
+				}
+				else{
+					dbConnection.commit();
+					dbConnection.setAutoCommit(true);
+				}
+			} catch (SQLException e) {
+				throw new RuntimeException(e);
+			}
+			finally{
+				if(pstmt != null){
+					try {
+						pstmt.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			
+			if(lastReadTransactionId!=null && (lastReadTransactionId!=null && !lastReadTransactionId.equals("unknown")) ) {
+			//updating the transaction_id for QC
+			System.out.println(qcSql);	
+			try {
+				if (!jdbcConnection.isConnected()) {
+					jdbcConnection.connect();
+				}
+				Connection dbConnection = jdbcConnection.getConnection();
+				dbConnection.setAutoCommit(false);
+				pstmt = dbConnection.prepareStatement(qcSql);
+				pstmt.setString(1, lastReadTransactionId);
+				pstmt.setString(2, sourceRepositoryID);
+				pstmt.setString(3, targetRepositoryID);
+				pstmt.setString(4, sourceArtifactID);
+				pstmt.setString(5, targetArtifactID);
+				int numRecordsAffected = pstmt.executeUpdate();
 				if(numRecordsAffected > 1){
 					dbConnection.rollback();
 					throw new RuntimeException("How come I am updating two repository mapping....?"+numRecordsAffected);
@@ -87,6 +141,10 @@ public class MappingDBUpdater implements IDataProcessor{
 					}
 				}
 			}
+			}
+			
+			
+			
 		}
 		else {
 			String message = "The Mapping updater needs a GenericArtifact object";
