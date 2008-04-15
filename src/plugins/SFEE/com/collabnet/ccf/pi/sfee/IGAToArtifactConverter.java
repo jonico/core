c1466372d228ca27df72dd72e755fe785987b5bd
mseethar <mseethar@collab.net>
2008-04-15 10:13:20 +0000

    Changed the package name to v44
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk@239 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- src/plugins/SFEE/com/collabnet/ccf/pi/sfee/IGAToArtifactConverter.java
+++
@@ -1,9 +0,0 @@
-package com.collabnet.ccf.pi.sfee;
-
-import org.dom4j.Document;
-
-import com.collabnet.ccf.core.ga.GenericArtifact;
-import com.vasoftware.sf.soap44.webservices.sfmain.TrackerFieldSoapDO;
-public interface IGAToArtifactConverter {
-	public Object convert(GenericArtifact dataObject, TrackerFieldSoapDO[] flexFields);
-}
