ebaeb81b140cd500a629a52d2779d59d0411927b
mseethar <mseethar@collab.net>
2008-04-16 09:47:34 +0000

    Moving all the SFEE plugin files into v44
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk@282 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- src/plugins/SFEE/com/collabnet/ccf/pi/sfee/v44/IArtifactToGAConverter.java
+++
@@ -1,7 +0,0 @@
-package com.collabnet.ccf.pi.sfee.v44;
-
-import com.collabnet.ccf.core.ga.GenericArtifact;
-import com.vasoftware.sf.soap44.webservices.sfmain.TrackerFieldSoapDO;
-public interface IArtifactToGAConverter {
-	public GenericArtifact convert(Object dataObject, TrackerFieldSoapDO[] trackerFields);
-}
