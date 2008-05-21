0759385f66167d7580d493d61afdfbf1e87c3df3
mseethar <mseethar@collab.net>
2008-05-21 07:17:24 +0000

    Modified the ConnectionFactory to return a new connection for each request. Now the ConnectionFactory will not cache a single connection and return the same for every project and domain parameters for a QC system.
    The Connection pooling mechanism takes care of pooling the connections and return them to the QCReader or QCWriter.
    
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk@476 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- src/plugins/HPQC/v90/com/collabnet/ccf/pi/qc/v90/api/ConnectionFactory.java
+++ src/plugins/HPQC/v90/com/collabnet/ccf/pi/qc/v90/api/ConnectionFactory.java
@@ -3,19 +3,17 @@ package com.collabnet.ccf.pi.qc.v90.api;
 import com.collabnet.ccf.pi.qc.v90.api.dcom.Connection;
 
 public class ConnectionFactory {
-	private static IConnection instance ;
 	@SuppressWarnings("unchecked")
 	public static IConnection getInstance(String server, String domain, String project, String user, String pass) {
-		if( instance == null || !instance.isLoggedIn() ) {
-			String clz = "com.collabnet.ccf.pi.qc.v90.api.dcom.Connection";
-			if( clz == null || clz.trim().length() == 0 ) clz = Connection.class.getCanonicalName();
-			try {
-				Class[] types = new Class[]{String.class,String.class,String.class,String.class,String.class};
-				Object[] params = new Object[]{server,domain,project,user,pass};
-				instance = (IConnection) Class.forName(clz).getConstructor(types).newInstance(params);
-			} catch (Exception e) {
-				throw new RuntimeException(e);
-			}
+		IConnection instance = null;
+		String clz = "com.collabnet.ccf.pi.qc.v90.api.dcom.Connection";
+		if( clz == null || clz.trim().length() == 0 ) clz = Connection.class.getCanonicalName();
+		try {
+			Class[] types = new Class[]{String.class,String.class,String.class,String.class,String.class};
+			Object[] params = new Object[]{server,domain,project,user,pass};
+			instance = (IConnection) Class.forName(clz).getConstructor(types).newInstance(params);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
 		}
 		return instance;
 	}
