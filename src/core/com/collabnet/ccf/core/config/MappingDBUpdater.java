c92044d6fbc4d627bfc1d8dff259645d44161986
mseethar <mseethar@collab.net>
2008-05-21 07:29:44 +0000

    Moved the database access code into Open Adaptor classes.
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk@485 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- src/core/com/collabnet/ccf/core/config/MappingDBUpdater.java
+++ src/core/com/collabnet/ccf/core/config/MappingDBUpdater.java
@@ -1,14 +1,12 @@
 package com.collabnet.ccf.core.config;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
 import org.dom4j.Document;
-import org.openadaptor.auxil.connector.jdbc.JDBCConnection;
+import org.openadaptor.auxil.connector.jdbc.writer.JDBCWriteConnector;
+import org.openadaptor.auxil.orderedmap.IOrderedMap;
+import org.openadaptor.auxil.orderedmap.OrderedHashMap;
 import org.openadaptor.core.IDataProcessor;
 
 import com.collabnet.ccf.core.ga.GenericArtifact;
@@ -17,27 +15,7 @@ import com.collabnet.ccf.core.ga.GenericArtifactParsingException;
 import com.collabnet.ccf.core.utils.DateUtil;
 
 public class MappingDBUpdater implements IDataProcessor{
-	JDBCConnection jdbcConnection = null;
-	String sql = "UPDATE REPOSITORY_MAPPING SET LAST_READ_TIME=? WHERE ID=(SELECT REPOSITORY_MAPPING.ID" 
-					+" FROM REPOSITORY_MAPPING, REPOSITORY_INFO A, REPOSITORY_INFO B, ARTIFACT_MAPPING WHERE"
-					+" A.ID=REPOSITORY_MAPPING.SOURCE_REPOSITORY_INFO_ID AND"
-					+" B.ID=REPOSITORY_MAPPING.TARGET_REPOSITORY_INFO_ID AND"
-					+" A.REPOSITORY_ID=? AND"
-					+" B.REPOSITORY_ID=? AND"
-					+" REPOSITORY_MAPPING.ID=ARTIFACT_MAPPING.MAPPING_ID AND"
-					+" ARTIFACT_MAPPING.SOURCE_ARTIFACT_ID=? AND"
-					+" ARTIFACT_MAPPING.TARGET_ARTIFACT_ID=?)";
-	
-	String qcSql = "UPDATE REPOSITORY_MAPPING SET TRANSACTION_ID=? WHERE ID=(SELECT REPOSITORY_MAPPING.ID" 
-		+" FROM REPOSITORY_MAPPING, REPOSITORY_INFO A, REPOSITORY_INFO B, ARTIFACT_MAPPING WHERE"
-		+" A.ID=REPOSITORY_MAPPING.SOURCE_REPOSITORY_INFO_ID AND"
-		+" B.ID=REPOSITORY_MAPPING.TARGET_REPOSITORY_INFO_ID AND"
-		+" A.REPOSITORY_ID=? AND"
-		+" B.REPOSITORY_ID=? AND"
-		+" REPOSITORY_MAPPING.ID=ARTIFACT_MAPPING.MAPPING_ID AND"
-		+" ARTIFACT_MAPPING.SOURCE_ARTIFACT_ID=? AND"
-		+" ARTIFACT_MAPPING.TARGET_ARTIFACT_ID=?)";
-	
+	private JDBCWriteConnector mappingWriter = null;
 	public Object[] process(Object data) {
 		// I will expect a Generic Artifact object
 		if(data instanceof Document){
@@ -56,129 +34,45 @@ public class MappingDBUpdater implements IDataProcessor{
 			
 			String lastModifiedDateString = ga.getArtifactLastModifiedDate();
 			String lastReadTransactionId = ga.getLastReadTransactionId();
-			String sourceRepositoryID = ga.getSourceRepositoryId();
-			String targetRepositoryID = ga.getTargetRepositoryId();
-			String sourceArtifactID = ga.getSourceArtifactId();
-			String targetArtifactID = ga.getTargetArtifactId();
+			String version = ga.getArtifactVersion();
+			String sourceRepositoryId = ga.getSourceRepositoryId();
+			String targetRepositoryId = ga.getTargetRepositoryId();
+			String sourceArtifactId = ga.getSourceArtifactId();
+			String targetArtifactId = ga.getTargetArtifactId();
 			java.util.Date lastModifiedDate = null;
 			if(lastModifiedDateString.equalsIgnoreCase("Unknown")){
-				lastModifiedDate = new Date();
 				return new Object[]{};
 			}
 			else {
 				lastModifiedDate = DateUtil.parse(lastModifiedDateString);
 			}
 			java.sql.Timestamp time = new java.sql.Timestamp(lastModifiedDate.getTime());
-			PreparedStatement pstmt = null;;
-			try {
-				if (!jdbcConnection.isConnected()) {
-					jdbcConnection.connect();
-				}
-				Connection dbConnection = jdbcConnection.getConnection();
-				dbConnection.setAutoCommit(false);
-				pstmt = dbConnection.prepareStatement(sql);
-				pstmt.setTimestamp(1, time);
-				pstmt.setString(2, sourceRepositoryID);
-				pstmt.setString(3, targetRepositoryID);
-				pstmt.setString(4, sourceArtifactID);
-				pstmt.setString(5, targetArtifactID);
-				int numRecordsAffected = pstmt.executeUpdate();
-				if(numRecordsAffected > 1){
-					dbConnection.rollback();
-					throw new RuntimeException("How come I am updating two repository mapping....?");
-				}
-				else{
-					dbConnection.commit();
-					dbConnection.setAutoCommit(true);
-				}
-			} catch (SQLException e) {
-				throw new RuntimeException(e);
-			}
-			finally{
-				if(pstmt != null){
-					try {
-						pstmt.close();
-					} catch (SQLException e) {
-						e.printStackTrace();
-					}
-				}
-			}
 			
-			if(!StringUtils.isEmpty(lastReadTransactionId)&& (lastReadTransactionId!=null && !lastReadTransactionId.equals("unknown")) ) {
-			//updating the transaction_id for QC
-			System.out.println(qcSql);	
-			try {
-				if (!jdbcConnection.isConnected()) {
-					jdbcConnection.connect();
-				}
-				Connection dbConnection = jdbcConnection.getConnection();
-				dbConnection.setAutoCommit(false);
-				pstmt = dbConnection.prepareStatement(qcSql);
-				pstmt.setString(1, lastReadTransactionId);
-				pstmt.setString(2, sourceRepositoryID);
-				pstmt.setString(3, targetRepositoryID);
-				pstmt.setString(4, sourceArtifactID);
-				pstmt.setString(5, targetArtifactID);
-				int numRecordsAffected = pstmt.executeUpdate();
-				if(numRecordsAffected > 1){
-					dbConnection.rollback();
-					throw new RuntimeException("How come I am updating two repository mapping....?"+numRecordsAffected);
-				}
-				else{
-					dbConnection.commit();
-					dbConnection.setAutoCommit(true);
-				}
-			} catch (SQLException e) {
-				throw new RuntimeException(e);
+			IOrderedMap inputParameters = new OrderedHashMap();
+			inputParameters.add(0,"LAST_READ_TIME",time);
+			int transactionId = 0;
+			if(StringUtils.isEmpty(lastReadTransactionId) || lastReadTransactionId.equals("unknown")){
+				transactionId = 0;
 			}
-			finally{
-				if(pstmt != null){
-					try {
-						pstmt.close();
-					} catch (SQLException e) {
-						e.printStackTrace();
-					}
+			else {
+				try{
+					transactionId = Integer.parseInt(lastReadTransactionId);
+				}
+				catch(NumberFormatException e){
+					e.printStackTrace();
 				}
 			}
-			//Updating the last_read_time field for this mapping too
-			
-			try {
-				if (!jdbcConnection.isConnected()) {
-					jdbcConnection.connect();
-				}
-				Connection dbConnection = jdbcConnection.getConnection();
-				dbConnection.setAutoCommit(false);
-				pstmt = dbConnection.prepareStatement(sql);
-				pstmt.setTimestamp(1, time);
-				pstmt.setString(2, targetRepositoryID);
-				pstmt.setString(3, sourceRepositoryID);
-				pstmt.setString(4, targetArtifactID);
-				pstmt.setString(5, sourceArtifactID);
-				int numRecordsAffected = pstmt.executeUpdate();
-				if(numRecordsAffected > 1){
-					dbConnection.rollback();
-					throw new RuntimeException("How come I am updating two repository mapping....?");
-				}
-				else{
-					dbConnection.commit();
-					dbConnection.setAutoCommit(true);
-				}
-			} catch (SQLException e) {
-				throw new RuntimeException(e);
-			}
-			finally{
-				if(pstmt != null){
-					try {
-						pstmt.close();
-					} catch (SQLException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-			}
-			
-			
-			
+			inputParameters.add(1,"TRANSACTION_ID",transactionId);
+			inputParameters.add(2,"VERSION",version);
+			inputParameters.add(3,"A.REPOSITORY_ID",sourceRepositoryId);
+			inputParameters.add(4,"B.REPOSITORY_ID",targetRepositoryId);
+			inputParameters.add(5,"ARTIFACT_MAPPING.SOURCE_ARTIFACT_ID",sourceArtifactId);
+			inputParameters.add(6,"ARTIFACT_MAPPING.TARGET_ARTIFACT_ID",targetArtifactId);
+
+			IOrderedMap[] params = new IOrderedMap[]{inputParameters};
+			mappingWriter.connect();
+			mappingWriter.deliver(params);
+			mappingWriter.disconnect();
 		}
 		else {
 			String message = "The Mapping updater needs a GenericArtifact object";
@@ -200,12 +94,12 @@ public class MappingDBUpdater implements IDataProcessor{
 		
 	}
 
-	public JDBCConnection getJdbcConnection() {
-		return jdbcConnection;
+	public JDBCWriteConnector getMappingWriter() {
+		return mappingWriter;
 	}
 
-	public void setJdbcConnection(JDBCConnection dbConnection) {
-		this.jdbcConnection = dbConnection;
+	public void setMappingWriter(JDBCWriteConnector mappingWriter) {
+		this.mappingWriter = mappingWriter;
 	}
 
 }
