bc874116e9c63b039f3d5fb81a98b204723502e5
venugopala <venugopala@collab.net>
2008-06-06 11:07:38 +0000

    Simple batch file for combined scenario
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk@689 6771e1e2-0f46-0410-b5d7-d846ff89f743

---
+++ samples/SFEE2SFEE/combined-bidirectional-mirroring-with-unidirectional-transformation/RunCCFService.bat
@@ -0,0 +1,3 @@
+call ..\..\..\bin\setEnv.bat
+
+%JAVA_HOME%\bin\java.exe -cp config;%LIB_PATH% -Djava.ext.dirs=%LIB_PATH%;%EXT_LIB_PATH% org.openadaptor.spring.SpringAdaptor -config config\SFEE2SFEE.xml
