669558ee20be30ad41dec9042d7095237506591e
mseethar <mseethar@collab.net>
2008-06-10 15:14:24 +0000

    Removed the log4j.configuration system property as the log4j properties are moved to log4j.xml instead of the log4j.properties
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk@822 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- samples/SFEE2SFEE/bidirectional-mirroring/RunCCFService.bat
+++ samples/SFEE2SFEE/bidirectional-mirroring/RunCCFService.bat
@@ -1,3 +1,3 @@
 call ..\..\..\bin\setEnv.bat
 
-%JAVA_HOME%\bin\java.exe -cp config;%LIB_PATH% -Djava.ext.dirs=%LIB_PATH%;%EXT_LIB_PATH% -Dlog4j.configuration=%LIB_PATH%\log4j.properties org.openadaptor.spring.SpringAdaptor -config config\SFEE2SFEE.xml
+%JAVA_HOME%\bin\java.exe -cp config;%LIB_PATH% -Djava.ext.dirs=%LIB_PATH%;%EXT_LIB_PATH% org.openadaptor.spring.SpringAdaptor -config config\SFEE2SFEE.xml
