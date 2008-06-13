66c5c76fb1179ac7c08aac07f7075b27e42fe6ed
mseethar <mseethar@collab.net>
2008-06-13 07:13:59 +0000

    Added heap size parameters to the JVM.
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk@884 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- samples/SFEE2SFEE/databaseTableImport/RunCCFService.bat
+++ samples/SFEE2SFEE/databaseTableImport/RunCCFService.bat
@@ -1,3 +1,3 @@
 call ..\..\..\bin\setEnv.bat
 
-%JAVA_HOME%\bin\java.exe -cp config;%LIB_PATH% -Djava.ext.dirs=%LIB_PATH%;%EXT_LIB_PATH% org.openadaptor.spring.SpringAdaptor -config config\database-table-import.xml
+%JAVA_HOME%\bin\java.exe -Xms512m -Xmx1024m -cp config;%LIB_PATH% -Djava.ext.dirs=%LIB_PATH%;%EXT_LIB_PATH% org.openadaptor.spring.SpringAdaptor -config config\database-table-import.xml
