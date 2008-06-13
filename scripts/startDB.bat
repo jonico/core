a5109ef80c1f7715a6bcedc49d22e9302874e133
mseethar <mseethar@collab.net>
2008-06-13 08:00:15 +0000

    Added JAVA_HOME in the JVM launch.
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk@901 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- scripts/startDB.bat
+++ scripts/startDB.bat
@@ -1,3 +1,5 @@
 rem This is the startup script for the CCF Database
 set CLASSPATH=..\lib\extlib\hsqldb.jar
-java org.hsqldb.Server -database.0 db/CCFDB -dbname.0 xdb > logs\db.log
+call ..\bin\setEnv.bat
+
+%JAVA_HOME%\bin\java org.hsqldb.Server -database.0 db/CCFDB -dbname.0 xdb > logs\db.log
