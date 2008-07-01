ad8cc3bea2177d138d57f83b7a6215966f2e4796
mseethar <mseethar@collab.net>
2008-07-01 05:27:44 +0000

    Fix for compilation error.
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk@1071 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- tests/com/collabnet/ccf/pi/sfee/v44/SFEEReaderTest.java
+++ tests/com/collabnet/ccf/pi/sfee/v44/SFEEReaderTest.java
@@ -3,9 +3,13 @@ package com.collabnet.ccf.pi.sfee.v44;
 import java.rmi.RemoteException;
 import java.util.Date;
 
+import org.dom4j.Document;
+
 import junit.framework.TestCase;
 
 import com.collabnet.ccf.core.eis.connection.ConnectionManager;
+import com.vasoftware.sf.soap44.types.SoapFilter;
+import com.vasoftware.sf.soap44.types.SoapSortKey;
 import com.vasoftware.sf.soap44.webservices.ClientSoapStubFactory;
 import com.vasoftware.sf.soap44.webservices.filestorage.ISimpleFileStorageAppSoap;
 import com.vasoftware.sf.soap44.webservices.sfmain.AttachmentSoapList;
@@ -13,6 +17,7 @@ import com.vasoftware.sf.soap44.webservices.sfmain.AttachmentSoapRow;
 import com.vasoftware.sf.soap44.webservices.sfmain.CommentSoapList;
 import com.vasoftware.sf.soap44.webservices.sfmain.CommentSoapRow;
 import com.vasoftware.sf.soap44.webservices.sfmain.ISourceForgeSoap;
+import com.vasoftware.sf.soap44.webservices.tracker.ArtifactDetailSoapRow;
 import com.vasoftware.sf.soap44.webservices.tracker.ArtifactSoapDO;
 import com.vasoftware.sf.soap44.webservices.tracker.ITrackerAppSoap;
 
@@ -49,7 +54,7 @@ public class SFEEReaderTest extends TestCase {
 	}
 
 	@SuppressWarnings("deprecation")
-	public void testReadTrackerItems(){
+	public void dummyTestReadTrackerItems(){
 		String projectTracker = "tracker1001";
 		repositoryId = projectTracker;
 		//"Mon Nov 05 00:00:00 GMT+05:30 2007"
@@ -57,11 +62,11 @@ public class SFEEReaderTest extends TestCase {
 		//Date lastModifiedDate = new Date(2008-1900,4,7,9,10,0);
 		boolean firstTimeImport = false;
 		Connection connection = sfeeReader.connect(systemId, systemKind, repositoryId, repositoryKind, connectionInfo, credentialInfo);
-		sfeeReader.readTrackerItems(projectTracker, lastModifiedDate, firstTimeImport, null,connection);
+		sfeeReader.readTrackerItems(projectTracker, lastModifiedDate, firstTimeImport, (Document) null,connection);
 		sfeeReader.disconnect(connection);
 	}
 	
-	public void te2stCommentsList(){
+	public void dummyTestCommentsList(){
 		Connection connection = sfeeReader.connect(systemId, systemKind, repositoryId, repositoryKind, connectionInfo, credentialInfo);
 		ISourceForgeSoap soapApp = connection.getSfSoap();
 		try {
@@ -84,7 +89,7 @@ public class SFEEReaderTest extends TestCase {
 		}
 	}
 	
-	public void te2stWriteAttachment(){
+	public void dummyTestWriteAttachment(){
 		Connection connection = sfeeReader.connect(systemId, systemKind, repositoryId, repositoryKind, connectionInfo, credentialInfo);
 		ISimpleFileStorageAppSoap fileStorageApp = (ISimpleFileStorageAppSoap) ClientSoapStubFactory.getSoapStub(
 				ISimpleFileStorageAppSoap.class, serverUrl);
@@ -108,7 +113,7 @@ public class SFEEReaderTest extends TestCase {
 		}
 	}
 	
-	public void te2stAttachmentsList(){
+	public void dummyTestAttachmentsList(){
 		Connection connection = sfeeReader.connect(systemId, systemKind, repositoryId, repositoryKind, connectionInfo, credentialInfo);
 		ISourceForgeSoap soapApp = connection.getSfSoap();
 		try {
@@ -125,6 +130,43 @@ public class SFEEReaderTest extends TestCase {
 			e.printStackTrace();
 		} finally{
 			sfeeReader.disconnect(connection);
+		}
+	}
+	
+	public void dummyTestArtfactRetreival() throws RemoteException{
+		repositoryId = "tracker1003";
+		Connection connection = sfeeReader.connect(systemId, systemKind, repositoryId, repositoryKind, connectionInfo, credentialInfo);
+		ISourceForgeSoap soapApp = connection.getSfSoap();
+		Date lastModifiedDate = new Date(2008-1900,5,16,16,45,0);
+		ITrackerAppSoap trackerApp = (ITrackerAppSoap) ClientSoapStubFactory.getSoapStub(
+				ITrackerAppSoap.class, serverUrl);
+		String[] selectedColumns = { ArtifactSoapDO.COLUMN_ID,
+				ArtifactSoapDO.COLUMN_LAST_MODIFIED_DATE,
+				ArtifactSoapDO.COLUMN_VERSION };
+		SoapSortKey[] sortKeys = { new SoapSortKey(
+				ArtifactSoapDO.COLUMN_LAST_MODIFIED_DATE, true) };
+		SoapFilter[] filter = { new SoapFilter("modifiedAfter",
+				SoapFilter.DATE_FORMAT.format(lastModifiedDate)) };
+		ArtifactDetailSoapRow[] rows = trackerApp.getArtifactDetailList(
+				connection.getSessionId(), repositoryId, selectedColumns, filter, sortKeys, 0, -1,
+				false, false).getDataRows();
+		for(ArtifactDetailSoapRow row:rows){
+			Date lmDate = row.getLastModifiedDate();
+			lmDate = new Date(lmDate.getTime()+1);
+			String[] selectedColumns1 = { ArtifactSoapDO.COLUMN_ID,
+					ArtifactSoapDO.COLUMN_LAST_MODIFIED_DATE,
+					ArtifactSoapDO.COLUMN_VERSION };
+			SoapSortKey[] sortKeys1 = { new SoapSortKey(
+					ArtifactSoapDO.COLUMN_LAST_MODIFIED_DATE, true) };
+			SoapFilter[] filter1 = { new SoapFilter("modifiedAfter",
+					SoapFilter.DATE_FORMAT.format(lmDate)) };
+			ArtifactDetailSoapRow[] rows1 = trackerApp.getArtifactDetailList(
+					connection.getSessionId(), repositoryId, selectedColumns1, filter1, sortKeys1, 0, -1,
+					false, false).getDataRows();
+			assertEquals(rows.length-1, rows1.length);
+		}
+		if(rows != null){
+			System.out.println("There were " + rows.length +" artifacts changed");
 		}
 	}
 	
