808b8a118ae0a57e58ce009def7018b8ff3afa48
mseethar <mseethar@collab.net>
2008-07-02 17:37:22 +0000

    Changed the startup script for the renamed wiring configuration file.
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk@1120 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- samples/IntegrationScenarios/flatFileExport/RunCCFService.bat
+++ samples/IntegrationScenarios/flatFileExport/RunCCFService.bat
@@ -1,3 +1,3 @@
 call ..\..\..\bin\setEnv.bat
 
-%JAVA_HOME%\bin\java.exe -Xms512m -Xmx1024m -cp config;%LIB_PATH% -Djava.ext.dirs=%LIB_PATH%;%EXT_LIB_PATH% org.openadaptor.spring.SpringAdaptor -config config\SFEE2FLATFILE.xml
+%JAVA_HOME%\bin\java.exe -Xms512m -Xmx1024m -cp config;%LIB_PATH% -Djava.ext.dirs=%LIB_PATH%;%EXT_LIB_PATH% org.openadaptor.spring.SpringAdaptor -config config\config.xml
