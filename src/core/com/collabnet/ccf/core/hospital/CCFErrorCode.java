9e891e7bf0a37c5b980151c7105421385edcbdb4
venugopala <venugopala@collab.net>
2008-07-03 11:00:47 +0000

    Included the EXTERNAL_SYSTEM_WRITE_ERROR for any write errors of defect or attachments in an external system.
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk@1147 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- src/core/com/collabnet/ccf/core/hospital/CCFErrorCode.java
+++ src/core/com/collabnet/ccf/core/hospital/CCFErrorCode.java
@@ -1,9 +1,10 @@
 package com.collabnet.ccf.core.hospital;
 
 public final class CCFErrorCode {
-	public static int SUCCESS = 0;
-	public static int GENERIC_ARTIFACT_NOT_SCHEMA_COMPLIANT = 1;
-	public static int GENERIC_ARTIFACT_PARSING_ERROR = 2;
-	public static int EXTERNAL_SYSTEM_CONNECTION_ERROR = 3;
-	public static int MAX_CONNECTIONS_REACHED_FOR_POOL = 4;
+	public final static int SUCCESS = 0;
+	public final static int GENERIC_ARTIFACT_NOT_SCHEMA_COMPLIANT = 1;
+	public final static int GENERIC_ARTIFACT_PARSING_ERROR = 2;
+	public final static int EXTERNAL_SYSTEM_CONNECTION_ERROR = 3;
+	public final static int MAX_CONNECTIONS_REACHED_FOR_POOL = 4;
+	public final static int EXTERNAL_SYSTEM_WRITE_ERROR = 5;
 }
