4716774d5b2afc99faa470fbdc64fce6527f3864
venugopala <venugopala@collab.net>
2008-07-04 04:30:29 +0000

    Included the TRANSFORMER_ERRORs
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk@1153 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- src/core/com/collabnet/ccf/core/hospital/CCFErrorCode.java
+++ src/core/com/collabnet/ccf/core/hospital/CCFErrorCode.java
@@ -7,4 +7,6 @@ public final class CCFErrorCode {
 	public final static int EXTERNAL_SYSTEM_CONNECTION_ERROR = 3;
 	public final static int MAX_CONNECTIONS_REACHED_FOR_POOL = 4;
 	public final static int EXTERNAL_SYSTEM_WRITE_ERROR = 5;
+	public final static int TRANSFORMER_FILE_ERROR = 6;
+	public final static int TRANSFORMER_TRANSFORMATION_ERROR = 7;
 }
