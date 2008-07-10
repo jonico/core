45d2de6980309c5a77e553678f4fab33cecc3b94
mseethar <mseethar@collab.net>
2008-07-10 06:18:23 +0000

    Added a stub implementation of the ConnectionFactory to return unique connection objects based on the input parameters.
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk@1220 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- tests/com/collabnet/ccf/core/eis/connection/MutliThreadedConnectionManagerTest.java
+++ tests/com/collabnet/ccf/core/eis/connection/MutliThreadedConnectionManagerTest.java
@@ -1,5 +1,8 @@
 package com.collabnet.ccf.core.eis.connection;
 
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
 
 import junit.framework.AssertionFailedError;
@@ -8,6 +11,9 @@ import junit.framework.TestResult;
 import org.jmock.Mock;
 import org.jmock.MockObjectTestCase;
 import org.jmock.core.Constraint;
+import org.jmock.core.Invocation;
+import org.jmock.core.Stub;
+import org.jmock.core.stub.CustomStub;
 
 /**
  * @author madhusuthanan
@@ -58,6 +64,40 @@ public class MutliThreadedConnectionManagerTest extends MockObjectTestCase {
 
 		final ConnectionManager<String> manager = new ConnectionManager<String>();
 		final int maxConnections = 25;
+		Stub connectionFactoryStub = new CustomStub("Returns a new string"){
+			private int i = 0;
+			private HashSet<String> connections = new HashSet<String>();
+			public Object invoke(Invocation invocation) throws Throwable {
+				Method invokedMethod = invocation.invokedMethod;
+				String name = invokedMethod.getName();
+				List params = invocation.parameterValues;
+				String returnValue = "";
+				if(name.contains("createConnection")){
+					for(Object param:params){
+						returnValue += param.toString();
+					}
+					returnValue += i++;
+				}
+				else if(name.contains("isAlive")){
+					String connection = (String) params.get(0);
+					if(connections.contains(connection)){
+						return true;
+					}
+					else{
+						return false;
+					}
+				}
+				else if(name.contains("closeConnection")){
+					String connection = (String) params.get(0);
+					Object closedConnection = connections.remove(connection);
+					if(closedConnection == null){
+						throw new ConnectionException("Trying to close a non-existent connection...!");
+					}
+					return null;
+				}
+				return returnValue;
+			}
+		};
 		manager.setMaxConnectionsPerPool(maxConnections);
 		manager.setMaxIdleTimeForConnection(50);
 		manager.setScavengerInterval(100);
@@ -68,10 +108,10 @@ public class MutliThreadedConnectionManagerTest extends MockObjectTestCase {
 				contains(repositoryId),
 				contains(repositoryKind),
 				contains(connectionInfo),
-				contains(credentialInfo)}).will(returnValue("CreatedConnection"));
-		context.expects(atLeastOnce()).method("isAlive").with(eq("CreatedConnection")).will(returnValue(true));
-		context.expects(atLeastOnce()).method("closeConnection").with(eq("CreatedConnection"));
-		final String[] connections = new String[numberOfConnectionsNeeded];
+				contains(credentialInfo)}).will(connectionFactoryStub);
+		context.expects(atLeastOnce()).method("isAlive").will(connectionFactoryStub);
+		context.expects(atLeastOnce()).method("closeConnection").will(connectionFactoryStub);
+		
 		final Random random = new Random(1058);
 		final TestCaseRunnable[] threads = new TestCaseRunnable[numberOfThreads * numberOfConnectionsNeeded];
 		for(int j=0; j < numberOfThreads; j++){
@@ -80,6 +120,7 @@ public class MutliThreadedConnectionManagerTest extends MockObjectTestCase {
 				final int iTmp = i;
 				threads[(jTmp+1)*(iTmp+1)-1] = 
 					new TestCaseRunnable(){
+					String connection = null;
 						public void runTestCase() throws ConnectionException{
 							try {
 									long randomLong1 = random.nextInt(20)*1000;
@@ -88,18 +129,21 @@ public class MutliThreadedConnectionManagerTest extends MockObjectTestCase {
 									} catch (InterruptedException e) {
 										e.printStackTrace();
 									}
-									connections[iTmp] = manager.getConnection(systemId+jTmp, systemKind+jTmp, repositoryId+jTmp,
+									connection = manager.getConnection(systemId+jTmp, systemKind+jTmp, repositoryId+jTmp,
 										repositoryKind+jTmp, connectionInfo+jTmp, credentialInfo+jTmp);
+									if(connection == null){
+										fail("Connection is null for " +iTmp+" "+jTmp);
+									}
 									long randomLong2 = random.nextInt(20)*1000;
 									try {
 										Thread.sleep(randomLong2);
 									} catch (InterruptedException e) {
 										e.printStackTrace();
 									}
-									manager.releaseConnection(connections[iTmp]);
-								} catch (MaxConnectionsReachedException e) {
-									fail("Max connections reached at "+(iTmp+1)
-											+" although the max connections configured is "+maxConnections);
+									manager.releaseConnection(connection);
+							} catch (MaxConnectionsReachedException e) {
+								fail("Max connections reached at "+(iTmp+1)
+										+" although the max connections configured is "+maxConnections);
 							}
 						}
 					};
