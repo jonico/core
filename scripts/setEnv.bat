e6b952dde0e2e00c11b756c81294b7ad334f4848
mseethar <mseethar@collab.net>
2008-07-29 13:13:05 +0000

    Added additional checks for valid JDK directory is set for JAVA_HOME.
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk@1313 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- scripts/setEnv.bat
+++ scripts/setEnv.bat
@@ -1,3 +1,4 @@
+@echo off
 rem This is a common script file that sets the CLASSPATH and
 rem environment variables for the CollabNet Connector framework
 rem startup scripts.
@@ -11,13 +12,29 @@ rem You will need JDK 1.5 and above.
 rem Point the CCF_HOME to the directory where you have extracted the CCF
 
 rem If you haven't set the CCF_HOME directory it will be taken as the parent directory of the
-rem bin directory file.
+rem bin directory.
 
-set JAVA_HOME=
+set JAVA_HOME=C:\mount\Java\jdk1.6.0_05
 set CCF_HOME=
 
-set CCF_HOME=%~dp0..
+if (%JAVA_HOME%)==() goto NO_JAVA_HOME
+if not exist %JAVA_HOME%\bin\java.exe goto NO_PROPER_JAVA_HOME
+if (%CCF_HOME%)==() set CCF_HOME=%~dp0..
+if not exist %CCF_HOME% goto NO_PROPER_CCF_HOME
 
 set LIB_PATH=%CCF_HOME%\lib
 set EXT_LIB_PATH=%CCF_HOME%\lib\extlib
 
+goto :EOF
+
+:NO_JAVA_HOME
+echo JAVA_HOME is not set. Please point JAVA_HOME to the JDK directory in your machine
+goto :EOF
+
+:NO_PROPER_JAVA_HOME
+echo JAVA_HOME is not properly set. Please point JAVA_HOME to the JDK directory in your machine
+goto :EOF
+
+:NO_PROPER_CCF_HOME
+echo CCF_HOME is not properly set. Please point the CCF_HOME to the CCF installation directory
+goto :EOF
