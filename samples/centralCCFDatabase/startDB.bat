57ba09ffccd8abf6097817efb69aa8db0ffa5b3e
mseethar <mseethar@collab.net>
2008-08-05 06:44:06 +0000

    Added command to create the logs directory in the centralCCFDatabase directory during database startup.
    Fix for bug [ccf85].
    
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/branches/ccf_1.0.x@1332 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- samples/centralCCFDatabase/startDB.bat
+++ samples/centralCCFDatabase/startDB.bat
@@ -1,5 +1,8 @@
+@echo off
 rem This is the startup script for the CCF Database
 set CLASSPATH=..\..\lib\extlib\hsqldb.jar
 call ..\..\bin\setEnv.bat
+
+if not exist logs mkdir logs
 
 %JAVA_HOME%\bin\java org.hsqldb.Server -database.0 ccfDatabase -dbname.0 xdb > logs\db.log
