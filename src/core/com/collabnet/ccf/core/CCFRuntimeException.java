112fcb4f304859916a96844667f798f661fad218
mseethar <mseethar@collab.net>
2008-08-05 07:02:42 +0000

    Added Javadoc comment.
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk@1334 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- src/core/com/collabnet/ccf/core/CCFRuntimeException.java
+++ src/core/com/collabnet/ccf/core/CCFRuntimeException.java
@@ -1,11 +1,34 @@
 package com.collabnet.ccf.core;
 
+/**
+ * This exception is intended for the CCF components to throw
+ * when there is an application exception and it should be
+ * propogated to the hospital.
+ * 
+ * @author Madhusuthanan Seetharam (madhusuthanan@collab.net)
+ *
+ */
 public class CCFRuntimeException extends RuntimeException {
 	
 	private static final long serialVersionUID = 3190879430557600332L;
+	/**
+	 * Constructs a CCFRuntimeException with the cause
+	 * provided.
+	 * 
+	 * @param cause - A description of the exceptional condition
+	 */
 	public CCFRuntimeException(String cause){
 		super(cause);
 	}
+
+	/**
+	 * Constructs a CCFRuntimeException with the cause providedand
+	 * root cause exception.
+	 * 
+	 * @param cause - A description of the exceptional condition
+	 * @param exception - The root cause exception that should be 
+	 * 						sent to the hospital
+	 */
 	public CCFRuntimeException(String cause, Throwable exception){
 		super(cause, exception);
 	}
