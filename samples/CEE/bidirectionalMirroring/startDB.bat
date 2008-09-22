59217b92819470ed89579fec9bbf36a4ecc4e90e
mseethar <mseethar@collab.net>
2008-09-22 09:36:01 +0000

    QC to PT bidirectional shipment startup scripts
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk@1471 6771e1e2-0f46-0410-b5d7-d846ff89f743

---
+++ samples/CEE/bidirectionalMirroring/startDB.bat
@@ -0,0 +1,7 @@
+rem This is the startup script for the CCF Database
+set CLASSPATH=..\..\..\lib\extlib\hsqldb.jar
+call ..\..\..\bin\setEnv.bat
+
+if not exist logs mkdir logs
+
+%JAVA_HOME%\bin\java org.hsqldb.Server -database.0 db/db -dbname.0 xdb > logs\db.log
