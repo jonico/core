2af215cb75245b077a8ff229d41f09b3dfad2c45
mseethar <mseethar@collab.net>
2008-09-28 12:12:51 +0000

    Utility class for converting identifiers into file names.
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk@1500 6771e1e2-0f46-0410-b5d7-d846ff89f743

---
+++ src/core/com/collabnet/ccf/core/utils/CCFUtils.java
@@ -0,0 +1,32 @@
+package com.collabnet.ccf.core.utils;
+
+import org.dom4j.Document;
+import org.dom4j.Element;
+
+import com.collabnet.ccf.core.ga.GenericArtifactHelper;
+import com.collabnet.ccf.core.ga.GenericArtifactParsingException;
+
+public final class CCFUtils {
+	public static final String delimiter = "-";
+	public static final String[] replaceCharacters = new String[]{":","\\{", "\\}", "/","\\\\"};
+	public static String getTempFileName(Document document) throws GenericArtifactParsingException{
+		Element element = document.getRootElement();
+		String sourceSystemId = XPathUtils.getAttributeValue(element, GenericArtifactHelper.SOURCE_SYSTEM_ID);
+		String sourceRepositoryId = XPathUtils.getAttributeValue(element, GenericArtifactHelper.SOURCE_REPOSITORY_ID);
+		String targetSystemId = XPathUtils.getAttributeValue(element, GenericArtifactHelper.TARGET_SYSTEM_ID);
+		String targetRepositoryId = XPathUtils.getAttributeValue(element, GenericArtifactHelper.TARGET_REPOSITORY_ID);
+		
+		String repositoryKey = sourceSystemId + delimiter + sourceRepositoryId + delimiter
+						+ targetSystemId + delimiter + targetRepositoryId;
+		for(String replaceCharacter:replaceCharacters){
+			repositoryKey = repositoryKey.replaceAll(replaceCharacter, delimiter);
+		}
+		return repositoryKey;
+	}
+	public static String getTempFileName(String someString){
+		for(String replaceCharacter:replaceCharacters){
+			someString = someString.replaceAll(replaceCharacter, delimiter);
+		}
+		return someString;
+	}
+}
