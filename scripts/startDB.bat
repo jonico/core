9473d281e3396918e2f865f5aaa278acab2d999b
mseethar <mseethar@collab.net>
2008-11-18 03:16:37 +0000

    Removing the CCFDBService scripts
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk@1767 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- scripts/startDB.bat
+++
@@ -1,7 +0,0 @@
-rem This is the startup script for the CCF Database
-set CLASSPATH=..\lib\extlib\hsqldb.jar
-call ..\bin\setEnv.bat
-
-if not exist logs mkdir logs
-
-%JAVA_HOME%\bin\java org.hsqldb.Server -database.0 db/CCFDB -dbname.0 xdb > logs\db.log
