fa023fb8e9e6244d2b4e87acadc81ae395decf53
jonico <jnicolai@collab.net>
2008-11-18 19:50:59 +0000

    deleted CEE folder
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk@1801 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- samples/CEE/bidirectionalMirroring/startDB.bat
+++
@@ -1,7 +0,0 @@
-rem This is the startup script for the CCF Database
-set CLASSPATH=..\..\..\lib\extlib\hsqldb.jar
-call ..\..\..\bin\setEnv.bat
-
-if not exist logs mkdir logs
-
-"%JAVA_HOME%\bin\java.exe" org.hsqldb.Server -database.0 db/db -dbname.0 xdb > logs\db.log
--- samples/CEE/flatFileImport/RunCCFService.bat
+++
@@ -1,3 +0,0 @@
-call ..\..\..\bin\setEnv.bat
-
-"%JAVA_HOME%\bin\java.exe" -Xms512m -Xmx1024m -cp config;"%LIB_PATH%" -Djava.ext.dirs="%JAVA_HOME%\jre\lib\ext";"%LIB_PATH%";"%EXT_LIB_PATH%" org.openadaptor.spring.SpringAdaptor -config config\config.xml
