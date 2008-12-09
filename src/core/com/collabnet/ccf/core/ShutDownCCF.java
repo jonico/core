0bdb3fca9a7718c630fca4fd3b06364dc8e03d3f
jonico <jnicolai@collab.net>
2008-12-09 20:18:15 +0000

    Added shutdown class for the ccf that should be called by service wrapper
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk@1919 6771e1e2-0f46-0410-b5d7-d846ff89f743

---
+++ src/core/com/collabnet/ccf/core/ShutDownCCF.java
@@ -0,0 +1,15 @@
+package com.collabnet.ccf.core;
+
+/**
+ * This class is called by ServiceWrapper whenever
+ * the application should terminate.
+ *
+ * @author jnicolai
+ *
+ */
+public class ShutDownCCF {
+	public static void main(String[] args) {
+		// set appropriate variable to flush all buffers in the reader components 
+		AbstractReader.setShutDownConnector(true);
+	}
+}
