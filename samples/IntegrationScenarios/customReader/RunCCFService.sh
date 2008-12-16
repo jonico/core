83590897b58643a60ef07a52aa96f3c8ac4893af
jonico <jnicolai@collab.net>
2008-12-16 14:54:38 +0000

    SUBMITTED - task ccf258: Replay scenario and core scenarios should have service wrapper files as well http://ccf.open.collab.net/servlets/Scarab?id=ccf258 CCF core scenarios now also come with service wrapper files
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk@1965 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- samples/IntegrationScenarios/customReader/RunCCFService.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-. ../../../bin/setEnv.sh
-
-$JAVA_HOME/bin/java -Xms512m -Xmx1024m -cp config:$LIB_PATH:bin -Djava.ext.dirs=$LIB_PATH:$EXT_LIB_PATH org.openadaptor.spring.SpringAdaptor -config config/config.xml
