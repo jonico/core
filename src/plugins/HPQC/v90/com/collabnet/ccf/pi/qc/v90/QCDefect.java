766e991c548d91b06c9f144762fb0c45531177c6
jonico <jnicolai@collab.net>
2009-07-28 10:26:08 +0000

    ccf339 - Modification of multi users in QC(which shifts from TF) end ,leads to exception in SFEE writer. QCReader now should be able to handle multiple user fields
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk/core@2976 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- src/plugins/HPQC/v90/com/collabnet/ccf/pi/qc/v90/QCDefect.java
+++ src/plugins/HPQC/v90/com/collabnet/ccf/pi/qc/v90/QCDefect.java
@@ -40,7 +40,7 @@ import com.jacob.com.Dispatch;
 
 /**
  * @author Collabnet (c) 2008
- *
+ * 
  */
 public class QCDefect extends Bug implements IQCDefect {
 	/**
@@ -56,8 +56,8 @@ public class QCDefect extends Bug implements IQCDefect {
 	 * This is the maximum size of the aggregate of attachments allowed to be
 	 * shipped in one cycle.
 	 */
-	//private static final long maxAttachmentSizePerCycle = 5000000;
-	//private static long cumulativeAttachmentSize = 0;
+	// private static final long maxAttachmentSizePerCycle = 5000000;
+	// private static long cumulativeAttachmentSize = 0;
 	static final String CARDINALITY_GREATER_THAN_ONE = "GT1";
 	static final String sfColumnName = "SF_COLUMN_NAME";
 	static final String sfColumnType = "SF_COLUMN_TYPE";
@@ -85,27 +85,36 @@ public class QCDefect extends Bug implements IQCDefect {
 		GenericArtifact dummyArtifact = new GenericArtifact();
 		return dummyArtifact;
 	}
+
 	/**
-	 * Constructs the GenericArtifact Java object for the defect after getting the schema from getSchemaAttachment method
-	 * It also populates all the values into the defect artifact.
-	 *
-	 * The logic for including attachment fields have been DEPRECATED and not used. This piece of the code, however, has not been
-	 * removed since it might be needed in future if there are any changes with respect to making attachments as first class citizens.
-	 *
+	 * Constructs the GenericArtifact Java object for the defect after getting
+	 * the schema from getSchemaAttachment method It also populates all the
+	 * values into the defect artifact.
+	 * 
+	 * The logic for including attachment fields have been DEPRECATED and not
+	 * used. This piece of the code, however, has not been removed since it
+	 * might be needed in future if there are any changes with respect to making
+	 * attachments as first class citizens.
+	 * 
 	 * @param qcc
 	 * @param actionId
 	 * @param entityId
-	 * @param attachmentNames (DEPRECATED)
+	 * @param attachmentNames
+	 *            (DEPRECATED)
 	 * @param isResync
-	 * @return GenericArtifact
-	 * 			Containing all the field values.
+	 * @return GenericArtifact Containing all the field values.
 	 */
 	public GenericArtifact getGenericArtifactObject(IConnection qcc,
-			String actionId, String entityId, int commentDescriber, int commentQualifier, List<String> attachmentNames,
-			String syncInfoTransactionId, String connectorUser, QCDefectHandler defectHandler, String sourceSystemTimezone, boolean isResync) {
+			String actionId, String entityId, int commentDescriber,
+			int commentQualifier, List<String> attachmentNames,
+			String syncInfoTransactionId, String connectorUser,
+			QCDefectHandler defectHandler, String sourceSystemTimezone,
+			boolean isResync) {
 		genericArtifact = QCConfigHelper.getSchemaFields(qcc, isResync);
-		List<String> txnIds = defectHandler.getTransactionIdsInRange(qcc, Integer.parseInt(entityId), Integer
-				.parseInt(syncInfoTransactionId), Integer.parseInt(actionId), connectorUser);
+		List<String> txnIds = defectHandler.getTransactionIdsInRange(qcc,
+				Integer.parseInt(entityId), Integer
+						.parseInt(syncInfoTransactionId), Integer
+						.parseInt(actionId), connectorUser);
 
 		List<GenericArtifactField> allFields = genericArtifact
 				.getAllGenericArtifactFields();
@@ -120,20 +129,22 @@ public class QCDefect extends Bug implements IQCDefect {
 			if (thisFieldsDatatype
 					.equals(GenericArtifactField.FieldValueTypeValue.DATE)
 					|| thisFieldsDatatype
-					.equals(GenericArtifactField.FieldValueTypeValue.DATETIME)) {
-				String connectorSystemTimeZone = TimeZone.getDefault().getDisplayName(false, TimeZone.SHORT);
+							.equals(GenericArtifactField.FieldValueTypeValue.DATETIME)) {
+				String connectorSystemTimeZone = TimeZone.getDefault()
+						.getDisplayName(false, TimeZone.SHORT);
 				Date dateValue = getFieldAsDate(thisField.getFieldName());
-				if(dateValue != null){
-					if(DateUtil.isAbsoluteDateInTimezone(dateValue, connectorSystemTimeZone)){
-						dateValue = DateUtil.convertToGMTAbsoluteDate(dateValue, connectorSystemTimeZone);
-						thisField.setFieldValueType(GenericArtifactField.FieldValueTypeValue.DATE);
+				if (dateValue != null) {
+					if (DateUtil.isAbsoluteDateInTimezone(dateValue,
+							connectorSystemTimeZone)) {
+						dateValue = DateUtil.convertToGMTAbsoluteDate(
+								dateValue, connectorSystemTimeZone);
 						thisField
-						.setFieldValue(dateValue);
-					}
-					else {
-						thisField.setFieldValueType(GenericArtifactField.FieldValueTypeValue.DATETIME);
+								.setFieldValueType(GenericArtifactField.FieldValueTypeValue.DATE);
+						thisField.setFieldValue(dateValue);
+					} else {
 						thisField
-							.setFieldValue(dateValue);
+								.setFieldValueType(GenericArtifactField.FieldValueTypeValue.DATETIME);
+						thisField.setFieldValue(dateValue);
 					}
 				}
 			}
@@ -150,37 +161,33 @@ public class QCDefect extends Bug implements IQCDefect {
 			// INFO Changes for user attributes handling...
 			if (thisFieldsDatatype
 					.equals(GenericArtifactField.FieldValueTypeValue.USER)) {
-				String fieldValue = getFieldAsString(thisField
-						.getFieldName());
-				//String oldFieldValue = null;
-				/*if (StringUtils.isEmpty(fieldValue)) {
-					IRecordSet auditPropertiesRS = null;
-					try {
-						auditPropertiesRS = defectHandler
-								.getAuditPropertiesRecordSet(qcc, txnIds);
-						oldFieldValue = defectHandler.getOldFieldValue(
-								auditPropertiesRS, thisField.getFieldName());
-					} finally {
-						if (auditPropertiesRS != null) {
-							auditPropertiesRS.safeRelease();
-							auditPropertiesRS = null;
+				String fieldValue = getFieldAsString(thisField.getFieldName());
+
+				if (StringUtils.isEmpty(fieldValue) || fieldValue == null) {
+					thisField.setFieldValue(null);
+				} else {
+					StringTokenizer st = new StringTokenizer(fieldValue, ";");
+					boolean firstFieldValue = true;
+					while (st.hasMoreTokens()) {
+						String thisFieldValue = st.nextToken();
+						if (firstFieldValue) {
+							firstFieldValue = false;
+							thisField.setFieldValue(thisFieldValue);
+						} else {
+							if (!StringUtils.isEmpty(thisFieldValue)) {
+								GenericArtifactField field = genericArtifact
+										.addNewField(
+												thisField.getFieldName(),
+												GenericArtifactField.VALUE_FIELD_TYPE_FLEX_FIELD);
+								field
+										.setFieldValueType(GenericArtifactField.FieldValueTypeValue.USER);
+								field.setFieldValue(thisFieldValue);
+							}
 						}
 					}
-					if (!StringUtils.isEmpty(oldFieldValue)) {
-						thisField.setFieldValue(oldFieldValue);
-						thisField
-								.setFieldAction(GenericArtifactField.FieldActionValue.DELETE);
-					} else {
-						thisField.setFieldValue(fieldValue);
-					}
-				} else {
-					
-				}*/
-				if (StringUtils.isEmpty(fieldValue)) {
-					fieldValue=null;
 				}
-				thisField.setFieldValue(fieldValue);
 			}
+
 			if (thisFieldsDatatype
 					.equals(GenericArtifactField.FieldValueTypeValue.BASE64STRING)) {
 				thisField
@@ -188,9 +195,10 @@ public class QCDefect extends Bug implements IQCDefect {
 			}
 			if (thisFieldsDatatype
 					.equals(GenericArtifactField.FieldValueTypeValue.STRING)) {
-				if(thisField.getMaxOccurs() == GenericArtifactField.CARDINALITY_UNBOUNDED) {
+				if (thisField.getMaxOccurs() == GenericArtifactField.CARDINALITY_UNBOUNDED) {
 
-					String fieldValue = getFieldAsString(thisField.getFieldName());
+					String fieldValue = getFieldAsString(thisField
+							.getFieldName());
 					List<String> fieldValues = new ArrayList<String>();
 					int size = 0;
 					if (fieldValue != null) {
@@ -201,22 +209,24 @@ public class QCDefect extends Bug implements IQCDefect {
 						thisField.setFieldValue(fieldValues.get(0));
 					for (int sizeCnt = 1; sizeCnt < size; sizeCnt++) {
 						GenericArtifactField field;
-						field = genericArtifact.addNewField(thisField
-								.getFieldName(),GenericArtifactField.VALUE_FIELD_TYPE_FLEX_FIELD);
+						field = genericArtifact
+								.addNewField(
+										thisField.getFieldName(),
+										GenericArtifactField.VALUE_FIELD_TYPE_FLEX_FIELD);
 						field
 								.setFieldValueType(GenericArtifactField.FieldValueTypeValue.STRING);
 						field.setFieldValue(fieldValues.get(sizeCnt));
 					}
 
-				}
-				else {
+				} else {
 					String fieldName = thisField.getFieldName();
 					String fieldValueAsString = null;
 					if (fieldName.equals("BG_VTS")) {
 						Date dateFieldValue = getFieldAsDate(fieldName);
 						thisField.setFieldValue(dateFieldValue);
-						//thisField.setFieldValue(DateUtil.formatQCDate(dateFieldValue));
-						thisField.setFieldValueType(GenericArtifactField.FieldValueTypeValue.DATETIME);
+						// thisField.setFieldValue(DateUtil.formatQCDate(dateFieldValue));
+						thisField
+								.setFieldValueType(GenericArtifactField.FieldValueTypeValue.DATETIME);
 					} else {
 						fieldValueAsString = getFieldAsString(fieldName);
 						thisField.setFieldValue(fieldValueAsString);
@@ -226,15 +236,16 @@ public class QCDefect extends Bug implements IQCDefect {
 			}
 		}
 		// If this is a resync request do not ship comments
-		if(!isResync) {
+		if (!isResync) {
 			IRecordSet auditPropertiesRS = null;
 			String deltaComment = null;
 			try {
-				auditPropertiesRS = defectHandler.getAuditPropertiesRecordSet(qcc, txnIds);
-				deltaComment = defectHandler.getDeltaOfComment(auditPropertiesRS);
-			}
-			finally {
-				if(auditPropertiesRS != null) {
+				auditPropertiesRS = defectHandler.getAuditPropertiesRecordSet(
+						qcc, txnIds);
+				deltaComment = defectHandler
+						.getDeltaOfComment(auditPropertiesRS);
+			} finally {
+				if (auditPropertiesRS != null) {
 					auditPropertiesRS.safeRelease();
 				}
 			}
@@ -247,111 +258,82 @@ public class QCDefect extends Bug implements IQCDefect {
 
 	}
 
-
-	/*public boolean isMultiSelectField(String fieldName, IConnection qcc) {
-
-		String sql = "SELECT * FROM SYSTEM_FIELD WHERE SF_TABLE_NAME='BUG' AND SF_COLUMN_NAME='"+fieldName+"'";
-		int rsCount = 0;
-		IRecordSet rs = null;
-		try {
-			rs = QCDefectHandler.executeSQL(qcc, sql);
-			if(rs!=null)
-				rsCount = rs.getRecordCount();
-			if(rsCount > 0)
-			{
-				String columnType = rs.getFieldValueAsString(sfColumnType);
-				String editStyle = rs.getFieldValueAsString(sfEditStyle);
-				String isMultiValue = rs.getFieldValueAsString(sfIsMultiValue);
-
-				if(columnType.equals("char") && editStyle!=null && isMultiValue!=null && !StringUtils.isEmpty(isMultiValue) &&
-						isMultiValue.equals("Y")){
-					if(editStyle.equals("ListCombo") || editStyle.equals("TreeCombo")) {
-						return true;
-					}
-				}
-			}
-		}
-		finally {
-			if(rs != null) {
-				rs.safeRelease();
-			}
-		}
-
-		return false;
-	}*/
-
-
-
+	/*
+	 * public boolean isMultiSelectField(String fieldName, IConnection qcc) {
+	 * 
+	 * String sql =
+	 * "SELECT * FROM SYSTEM_FIELD WHERE SF_TABLE_NAME='BUG' AND SF_COLUMN_NAME='"
+	 * +fieldName+"'"; int rsCount = 0; IRecordSet rs = null; try { rs =
+	 * QCDefectHandler.executeSQL(qcc, sql); if(rs!=null) rsCount =
+	 * rs.getRecordCount(); if(rsCount > 0) { String columnType =
+	 * rs.getFieldValueAsString(sfColumnType); String editStyle =
+	 * rs.getFieldValueAsString(sfEditStyle); String isMultiValue =
+	 * rs.getFieldValueAsString(sfIsMultiValue);
+	 * 
+	 * if(columnType.equals("char") && editStyle!=null && isMultiValue!=null &&
+	 * !StringUtils.isEmpty(isMultiValue) && isMultiValue.equals("Y")){
+	 * if(editStyle.equals("ListCombo") || editStyle.equals("TreeCombo")) {
+	 * return true; } } } } finally { if(rs != null) { rs.safeRelease(); } }
+	 * 
+	 * return false; }
+	 */
 
 	/**
-	 * DEPRECATED: As the function is done by the QCAttachmentHandler class, this method is  not used right now.
-	 *
-	 * Constructs the GenericArtifact Java object for the attachment after getting the schema from getSchemaAttachment method
-	 * of the QCConfigHelper. It also populates all the values into the attachment artifact.
-	 *
+	 * DEPRECATED: As the function is done by the QCAttachmentHandler class,
+	 * this method is not used right now.
+	 * 
+	 * Constructs the GenericArtifact Java object for the attachment after
+	 * getting the schema from getSchemaAttachment method of the QCConfigHelper.
+	 * It also populates all the values into the attachment artifact.
+	 * 
 	 * @param qcc
 	 * @param actionId
 	 * @param entityId
 	 * @param attachmentNames
-	 * @return GenericArtifact
-	 * 			Containing all the field values.
+	 * @return GenericArtifact Containing all the field values.
 	 */
 	/*
-	public GenericArtifact getGenericArtifactObjectWithOnlyAttachments(
-			IConnection qcc, String actionId, String entityId,
-			List<String> attachmentNames) {
-
-		if (attachmentNames != null)
-			genericArtifact = QCConfigHelper.getCompleteSchemaAttachments(qcc, actionId, entityId, attachmentNames);
-
-		List<GenericArtifactAttachment> allAttachments = genericArtifact
-				.getAllGenericArtifactAttachments();
-		int noOfAttachments = 0;
-		if (allAttachments != null)
-			noOfAttachments = allAttachments.size();
-		for (int cnt = 0; cnt < noOfAttachments; cnt++) {
-
-			// if (attachOperation != null) {
-			// filling the values for attachment
-			IFactory bugFactory = qcc.getBugFactory();
-			IBug bug = bugFactory.getItem(entityId);
-
-			GenericArtifactAttachment thisAttachment = allAttachments.get(cnt);
-			MimetypesFileTypeMap mimeType = new MimetypesFileTypeMap();
-			String thisMimeType = mimeType.getContentType(thisAttachment
-					.getAttachmentName());
-			thisAttachment.setMimeType(thisMimeType);
-
-			if (thisAttachment.getAttachmentContentType().equals(
-					GenericArtifactAttachment.AttachmentContentTypeValue.DATA)) {
-				byte data[] = null;
-				try {
-					data = bug.retrieveAttachmentData(attachmentNames.get(cnt));
-				} catch (Exception e) {
-					log
-							.error("An Exception!!!!! occured in QCDefect while trying to do retrieveAttachmentData of an INVALID Filename"
-									+ e);
-					return genericArtifact;
-				}
-				log.info("************************************************");
-				long attachmentSize = (long) data.length;
-				thisAttachment.setAttachmentSize(attachmentSize);
-				thisAttachment.setRawAttachmentData(data);
-				thisAttachment.setAttachmentSourceUrl("VALUE_UNKNOWN");
-			} else {
-				thisAttachment.setAttachmentSourceUrl(attachmentNames.get(cnt));
-				thisAttachment.setAttachmentSize(0);
-			}
-		}
-		if (allAttachments != null) {
-			Boolean attachmentSizeChecker = checkForAttachmentSize(allAttachments);
-			if (attachmentSizeChecker == false)
-				return null;
-		}
-
-		return genericArtifact;
-	}
-	*/
+	 * public GenericArtifact getGenericArtifactObjectWithOnlyAttachments(
+	 * IConnection qcc, String actionId, String entityId, List<String>
+	 * attachmentNames) {
+	 * 
+	 * if (attachmentNames != null) genericArtifact =
+	 * QCConfigHelper.getCompleteSchemaAttachments(qcc, actionId, entityId,
+	 * attachmentNames);
+	 * 
+	 * List<GenericArtifactAttachment> allAttachments = genericArtifact
+	 * .getAllGenericArtifactAttachments(); int noOfAttachments = 0; if
+	 * (allAttachments != null) noOfAttachments = allAttachments.size(); for
+	 * (int cnt = 0; cnt < noOfAttachments; cnt++) {
+	 * 
+	 * // if (attachOperation != null) { // filling the values for attachment
+	 * IFactory bugFactory = qcc.getBugFactory(); IBug bug =
+	 * bugFactory.getItem(entityId);
+	 * 
+	 * GenericArtifactAttachment thisAttachment = allAttachments.get(cnt);
+	 * MimetypesFileTypeMap mimeType = new MimetypesFileTypeMap(); String
+	 * thisMimeType = mimeType.getContentType(thisAttachment
+	 * .getAttachmentName()); thisAttachment.setMimeType(thisMimeType);
+	 * 
+	 * if (thisAttachment.getAttachmentContentType().equals(
+	 * GenericArtifactAttachment.AttachmentContentTypeValue.DATA)) { byte data[]
+	 * = null; try { data =
+	 * bug.retrieveAttachmentData(attachmentNames.get(cnt)); } catch (Exception
+	 * e) { log.error(
+	 * "An Exception!!!!! occured in QCDefect while trying to do retrieveAttachmentData of an INVALID Filename"
+	 * + e); return genericArtifact; }
+	 * log.info("************************************************"); long
+	 * attachmentSize = (long) data.length;
+	 * thisAttachment.setAttachmentSize(attachmentSize);
+	 * thisAttachment.setRawAttachmentData(data);
+	 * thisAttachment.setAttachmentSourceUrl("VALUE_UNKNOWN"); } else {
+	 * thisAttachment.setAttachmentSourceUrl(attachmentNames.get(cnt));
+	 * thisAttachment.setAttachmentSize(0); } } if (allAttachments != null) {
+	 * Boolean attachmentSizeChecker = checkForAttachmentSize(allAttachments);
+	 * if (attachmentSizeChecker == false) return null; }
+	 * 
+	 * return genericArtifact; }
+	 */
 
 	public List<String> getFieldValues(String fieldValue) {
 
@@ -365,19 +347,14 @@ public class QCDefect extends Bug implements IQCDefect {
 	}
 
 	/*
-	public boolean checkForAttachmentSize(
-			List<GenericArtifactAttachment> allAttachments) {
-
-		for (int cnt = 0; cnt < allAttachments.size(); cnt++) {
-			long thisAttachmentSize = allAttachments.get(cnt)
-					.getAttachmentSize();
-			cumulativeAttachmentSize += thisAttachmentSize;
-		}
-		if (cumulativeAttachmentSize < maxAttachmentSizePerCycle)
-			return true;
-		else
-			return false;
-	}
-	*/
+	 * public boolean checkForAttachmentSize( List<GenericArtifactAttachment>
+	 * allAttachments) {
+	 * 
+	 * for (int cnt = 0; cnt < allAttachments.size(); cnt++) { long
+	 * thisAttachmentSize = allAttachments.get(cnt) .getAttachmentSize();
+	 * cumulativeAttachmentSize += thisAttachmentSize; } if
+	 * (cumulativeAttachmentSize < maxAttachmentSizePerCycle) return true; else
+	 * return false; }
+	 */
 
 }
