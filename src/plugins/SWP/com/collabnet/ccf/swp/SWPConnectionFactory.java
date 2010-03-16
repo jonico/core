b6ae76515ef2bd90cfd87ee777fccae8d975bb89
jonico <jnicolai@collab.net>
2010-03-16 20:16:01 +0000

    Implemented and documented all methods of the SWPConnectionFactory
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/branches/TF-SWP/core@3239 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- src/plugins/SWP/com/collabnet/ccf/swp/SWPConnectionFactory.java
+++ src/plugins/SWP/com/collabnet/ccf/swp/SWPConnectionFactory.java
@@ -1,5 +1,7 @@
 package com.collabnet.ccf.swp;
 
+import java.rmi.RemoteException;
+
 import javax.xml.rpc.ServiceException;
 
 import com.collabnet.ccf.core.eis.connection.ConnectionException;
@@ -7,31 +9,48 @@ import com.collabnet.ccf.core.eis.connection.ConnectionFactory;
 import com.collabnet.ccf.core.eis.connection.ConnectionManager;
 
 public class SWPConnectionFactory implements ConnectionFactory<Connection> {
-	
+
 	public static final String PARAM_DELIMITER = ":";
 
+	
+
+	/**
+	 * Closes SWP connection
+	 */
 	public void closeConnection(Connection connection)
 			throws ConnectionException {
-		// TODO Auto-generated method stub
-
+		// since there are no SWP sessions for WS calls, we do not have to do
+		// anything here
 	}
 
+	/**
+	 * Create an SWP connection object
+	 */
 	public Connection createConnection(String systemId, String systemKind,
 			String repositoryId, String repositoryKind, String connectionInfo,
 			String credentialInfo,
 			ConnectionManager<Connection> connectionManager)
 			throws ConnectionException {
-		// TODO implement me
-		
 		try {
-			return new Connection(repositoryId, repositoryKind, connectionInfo, credentialInfo, connectionManager);
+			return new Connection(repositoryId, repositoryKind, connectionInfo,
+					credentialInfo, connectionManager);
 		} catch (ServiceException e) {
-			throw new ConnectionException("Could not connect to SWP" , e);
+			throw new ConnectionException("Could not connect to SWP", e);
 		}
 	}
 
+	/**
+	 * Call method to check whether SWP connection still works
+	 */
 	public boolean isAlive(Connection connection) {
-		// TODO Auto-generated method stub
+		try {
+			connection.getEndpoint().getTest();
+		} catch (RemoteException e) {
+			// we return false because we received an exception indicating
+			// that this connection object no longer works
+			return false;
+		}
+		// we return true because the connection object still works
 		return true;
 	}
 
