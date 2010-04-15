67f3398c4f832992d9215e312367f8639f19792e
kdon <kdon@collab.net>
2010-04-15 18:17:38 +0000

    Tests that fields for a backlog item created in TeamForge is correctly synched to the backlog item in ScrumWorks
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/branches/TF-SWP/core@3372 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- tests/com/collabnet/ccf/integration/tfswp/TF2SWPTests.java
+++ tests/com/collabnet/ccf/integration/tfswp/TF2SWPTests.java
@@ -3,17 +3,22 @@
  */
 package com.collabnet.ccf.integration.tfswp;
 
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.*;
 
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.collabnet.ce.soap50.webservices.cemain.TrackerFieldSoapDO;
 import com.collabnet.teamforge.api.FieldValues;
 import com.danube.scrumworks.api.client.types.BacklogItemWSO;
 import com.danube.scrumworks.api.client.types.ProductWSO;
+import com.danube.scrumworks.api.client.types.ThemeWSO;
 
 /**
  * This class contains the tests from the TF to SWP
@@ -38,17 +43,38 @@ public class TF2SWPTests extends TFSWPIntegrationTest {
 		super.tearDown();
 	}
 
+	/**
+	 * Creates a backlog item in TeamForge and verifies the backlog item in ScrumWorks after the synchronization.   
+	 * Fields for the backlog item: title, description, benefit, penalty, effort, and product themes.  
+	 * 
+	 * @throws RemoteException if the test can not connect to the TeamForge API
+	 * @throws InterruptedException if the thread can not sleep
+	 */
 	@Test
-	public void testPBICreation() throws RemoteException, InterruptedException {
-		// create a tracker item in the TF PBI tracker, sleep some seconds
-		// and look whether a PBI with the same name had been created in SWP as
-		// well
-		cleanUpArtifacts();
-		FieldValues flexFields = new FieldValues();
+	public void testBacklogItemCreation() throws RemoteException, InterruptedException {
+		// execute
+		final String title = "TFPBI";
+		final String description = "TFPBIDescription";
+		final String benefit = "10";
+		final String penalty = "20";
+		final String effort = "30";
+		final String theme1 = "Core";
+		final String theme2 = "GUI";
+		final FieldValues flexFields = new FieldValues();
+		flexFields.setNames(new String[] {"Benefit", "Penalty", "Backlog Effort", "Themes", "Themes"}); 
+		flexFields.setValues(new String[] {benefit, penalty, effort, theme1, theme2} );
+		final int flexFieldsLength = flexFields.getNames().length;
+		final String[] flexFieldTypes = new String[flexFieldsLength];
+		for (int i = 0; i < flexFieldsLength; i++) {
+			flexFieldTypes[i] = TrackerFieldSoapDO.FIELD_VALUE_TYPE_STRING; 
+		}
+		flexFields.setTypes(flexFieldTypes); 
+		// TODO: assign planning folder in TeamForge (release)
+		
 		getTFConnection().getTrackerClient().createArtifact(getTfPBITracker(),
-				"TFPBI", "TFPBIDescription", null, null, "Open", null, 0, 0,
+				title, description, null, null, "Open", null, 0, 0,
 				42, false, null, null, null, flexFields, null, null, null);
-		ProductWSO product = getSWPEndpoint().getProductByName(getSwpProduct());
+		final ProductWSO product = getSWPEndpoint().getProductByName(getSwpProduct());
 		BacklogItemWSO[] pbis = null;
 		for (int i = 0; i < getCcfMaxWaitTime(); i+=getCcfRetryInterval() ) {
 			pbis = getSWPEndpoint().getActiveBacklogItems(product);
@@ -58,9 +84,25 @@ public class TF2SWPTests extends TFSWPIntegrationTest {
 				break;
 			}
 		}
+		
+		// verify
 		assertEquals(1, pbis.length);
 		BacklogItemWSO pbi = pbis[0];
-		assertEquals("TFPBI", pbi.getTitle());
-		assertEquals("TFPBIDescription", pbi.getDescription());
+		assertEquals(title, pbi.getTitle());
+		assertEquals(description, pbi.getDescription());
+		assertEquals(benefit, pbi.getBusinessWeight().getBenefit().toString()); 
+		assertEquals(penalty, pbi.getBusinessWeight().getPenalty().toString()); 
+		assertEquals(effort, pbi.getEstimate().toString());
+		final List<String> themeNames = new ArrayList<String>(); 
+		final ThemeWSO[] themes = pbi.getThemes();
+		assertEquals(2, themes.length);
+		for (int i = 0; i < themes.length; i++) {
+			themeNames.add(themes[i].getName()); 
+		}
+		assertTrue(themeNames.contains(theme1)); 
+		assertTrue(themeNames.contains(theme2)); 
+		
+		// teardown
+		cleanUpArtifacts(); 
 	}
 }
