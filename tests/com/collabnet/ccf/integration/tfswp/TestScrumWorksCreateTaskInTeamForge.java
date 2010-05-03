8a6086a84109e3fe2d4dc0280a92d8bee3ba8383
jonico <jnicolai@collab.net>
2010-05-03 14:57:04 +0000

    Made unit test reliable by ensuring that PBI will be synched before trying to create child task artifact
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/branches/TF-SWP/core@3481 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- tests/com/collabnet/ccf/integration/tfswp/TestScrumWorksCreateTaskInTeamForge.java
+++ tests/com/collabnet/ccf/integration/tfswp/TestScrumWorksCreateTaskInTeamForge.java
@@ -9,30 +9,36 @@ import com.collabnet.teamforge.api.tracker.ArtifactRow;
 import com.danube.scrumworks.api2.client.BacklogItem;
 
 /**
- * Tests that a task created in ScrumWorks is correctly synchronized in TeamForge. 
+ * Tests that a task created in ScrumWorks is correctly synchronized in
+ * TeamForge.
  * 
  * @author Kelley
- *
+ * 
  */
 public class TestScrumWorksCreateTaskInTeamForge extends TFSWPIntegrationTest {
 	private BacklogItem scrumWorksBacklogItem;
 	private final String release = SWPTester.RELEASE_1;
-	
+
 	@Override
 	@Before
 	public void setUp() throws Exception {
 		final String pbiTitle = "pbi";
-		
+
 		super.setUp();
 
-		scrumWorksBacklogItem = getSWPTester().createBacklogItem(pbiTitle, release);  
-		
+		scrumWorksBacklogItem = getSWPTester().createBacklogItem(pbiTitle,
+				release);
+		// before back log item did not appear, we could run in a
+		// parentArtifactNotPresentError
+		getTeamForgeTester().waitForBacklogItemsToAppear(1);
 	}
-	
+
 	/**
-	 * Tests creating a task in ScrumWorks and verifying that the task is created in TeamForge.
+	 * Tests creating a task in ScrumWorks and verifying that the task is
+	 * created in TeamForge.
 	 * 
-	 * @throws Exception if an error occurs
+	 * @throws Exception
+	 *             if an error occurs
 	 */
 	@Test
 	public void testCreateTask() throws Exception {
@@ -43,25 +49,32 @@ public class TestScrumWorksCreateTaskInTeamForge extends TFSWPIntegrationTest {
 		final TaskStatus status = TaskStatus.IN_PROGRESS;
 
 		// execute
-		getSWPTester().createTask(taskTitle, description, pointPerson, status, currentEstimate, scrumWorksBacklogItem.getId()); 
-	
-		// verify 
-		final ArtifactRow[] allTasks = getTeamForgeTester().waitForTasksToAppear(1);
-		final ArtifactRow task = allTasks[0]; 
-		
-		assertEquals(taskTitle, task.getTitle()); 
-		assertEquals(description, task.getDescription()); 
-		assertEquals(status.getStatus(), task.getStatus()); 
-		assertEquals("", task.getCategory()); 
+		getSWPTester().createTask(taskTitle, description, pointPerson, status,
+				currentEstimate, scrumWorksBacklogItem.getId());
+
+		// verify
+		final ArtifactRow[] allTasks = getTeamForgeTester()
+				.waitForTasksToAppear(1);
+		final ArtifactRow task = allTasks[0];
+
+		assertEquals(taskTitle, task.getTitle());
+		assertEquals(description, task.getDescription());
+		assertEquals(status.getStatus(), task.getStatus());
+		assertEquals("", task.getCategory());
 		assertEquals(0, task.getPriority());
-		assertEquals(pointPerson, task.getAssignedToFullname()); 
-		assertEquals(getTeamForgeTester().getPlanningFolderId(release), task.getPlanningFolderId()); 
-		assertEquals(Integer.parseInt(currentEstimate), task.getEstimatedEffort()); 
-		assertEquals(Integer.parseInt(currentEstimate), task.getRemainingEffort()); 
-		
-		final ArtifactRow[] allBacklogItems = getTeamForgeTester().waitForBacklogItemsToAppear(1); 
-		final ArtifactRow backlogItem = allBacklogItems[0]; 
-		assertEquals(backlogItem.getId(), getTeamForgeTester().getParentId(task.getId())); 
+		assertEquals(pointPerson, task.getAssignedToFullname());
+		assertEquals(getTeamForgeTester().getPlanningFolderId(release), task
+				.getPlanningFolderId());
+		assertEquals(Integer.parseInt(currentEstimate), task
+				.getEstimatedEffort());
+		assertEquals(Integer.parseInt(currentEstimate), task
+				.getRemainingEffort());
+
+		final ArtifactRow[] allBacklogItems = getTeamForgeTester()
+				.waitForBacklogItemsToAppear(1);
+		final ArtifactRow backlogItem = allBacklogItems[0];
+		assertEquals(backlogItem.getId(), getTeamForgeTester().getParentId(
+				task.getId()));
 	}
-	
+
 }
