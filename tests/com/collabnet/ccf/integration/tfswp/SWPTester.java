5b9a83e974498a0fcdbd827e9d5bb9eb488f52af
jonico <jnicolai@collab.net>
2010-05-04 11:09:49 +0000

    Inserted diagnostic message
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/branches/TF-SWP/core@3483 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- tests/com/collabnet/ccf/integration/tfswp/SWPTester.java
+++ tests/com/collabnet/ccf/integration/tfswp/SWPTester.java
@@ -277,7 +277,9 @@ public class SWPTester {
 			tasks = getSWPEndpoint().getTasks(backlogItem.getId());
 			if (tasks == null || tasks.size() < numberOfTasks
 					|| !tasks.get(0).getName().equals(expectedTaskTitle)) {
-				System.out.println(tasks.get(0).getName());
+				if (tasks != null) {
+					System.out.println(tasks.get(0).getName());
+				}
 				Thread.sleep(ccfRetryInterval);
 			} else {
 				assertEquals(numberOfTasks, tasks.size());
