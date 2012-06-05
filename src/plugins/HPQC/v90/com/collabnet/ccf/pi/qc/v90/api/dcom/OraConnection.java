9c0adcdb5e4e709d654e85403583a2bfa6186f0a
vzk <vkopetzky@collab.net>
2012-06-05 12:23:11 +0000

    patching the patch, what else? (refactored log/debug output)
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/branches/tdadminfix@5370 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- src/plugins/HPQC/v90/com/collabnet/ccf/pi/qc/v90/api/dcom/OraConnection.java
+++ src/plugins/HPQC/v90/com/collabnet/ccf/pi/qc/v90/api/dcom/OraConnection.java
@@ -9,7 +9,6 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.sql.ResultSetMetaData;
 import java.util.Properties;
 
 
@@ -44,10 +43,10 @@ public class OraConnection {
 			try {
 				oraProps.load( new FileInputStream( new File(url.getFile())));
 			} catch ( IOException e) {
-					log.fatal("Could not open oracle jdbc config file: " + url.getFile() , e);
+					log.error("Could not open oracle jdbc config file: " + url.getFile() , e);
 			}
 		} else {
-			log.fatal("Could not find oracle jdbc config file: is oracle.jdbc.properties present and in classpath?");
+			log.error("Could not find oracle jdbc config file: is oracle.jdbc.properties present and in classpath?");
 		}
 		
 		driverClassName = oraProps.getProperty(DRIVERKEY);
@@ -55,10 +54,16 @@ public class OraConnection {
 		username = oraProps.getProperty(USERNAMEKEY);
 		password = oraProps.getProperty(PASSWORDKEY);
 		
+		log.info( "succesfully loaded JDBC properties from " + url.getFile()  + nl + 
+				"Driver Class Name: " + driverClassName + nl + 
+				"Connection String: " + connectionString + nl + 
+				"User Name:         " + username + nl + 
+				"Password:          " + password );
+		
 		try {
 			Class.forName(driverClassName);
 		} catch (ClassNotFoundException e) {
-			log.fatal("Could not instantiate oracle driver " + driverClassName + ", is the file ojdbc6.jar present and in classpath?" , e);
+			log.error("Could not instantiate oracle driver " + driverClassName + ", is the file ojdbc6.jar present and in classpath?" , e);
 		}
 		
 		String current = "connection";
@@ -71,7 +76,7 @@ public class OraConnection {
 			log.error("Error Code: " + e.getErrorCode());
 			log.error("Message:    " + e.getMessage());
 			log.error("SQL State:  " + e.getSQLState());
-			log.fatal("No " + current , e);
+			log.error("No " + current , e);
 		}
 	}
 	public IRecordSet executeSql ( String sql ) {
@@ -84,7 +89,7 @@ public class OraConnection {
 			log.error("Error Code: " + e.getErrorCode());
 			log.error("Message:    " + e.getMessage());
 			log.error("SQL State:  " + e.getSQLState());
-			log.fatal("Failed SQL execution", e);
+			log.error("Failed SQL execution", e);
 		} 
 		
 		return new OracRecordSet( rs );
